175d00adb6510e3aada5665b400373c91ae88fb0
Chris Beer <cabeer@stanford.edu>
2016-03-28 15:23:16 -0700

    Generate sufia using the installer

--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,37 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root 'sufia/homepage#index'
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  mount Blacklight::Engine => '/'
+  mount Hydra::Collections::Engine => '/'
+  mount CurationConcerns::Engine, at: '/'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  concern :searchable, Blacklight::Routes::Searchable.new
+  concern :exportable, Blacklight::Routes::Exportable.new
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  devise_for :users
+  Hydra::BatchEdit.add_routes(self)
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  curation_concerns_collections
+  curation_concerns_basic_routes
+  curation_concerns_embargo_management
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+  resources :bookmarks do
+    concerns :exportable
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+    collection do
+      delete 'clear'
+    end
+  end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  # This must be the very last route in the file because it has a catch-all route for 404 errors.
+  # This behavior seems to show up only in production mode.
+  mount Sufia::Engine => '/'
 end
---
+++ spec/controllers/curation_concerns/generic_works_controller_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate curation_concerns:work GenericWork`
+require 'rails_helper'
+
+RSpec.describe CurationConcerns::GenericWorksController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
