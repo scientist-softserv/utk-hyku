7bdef2da066e0187ece1a1441cade579af212a7a
Chris Beer <cabeer@stanford.edu>
2016-03-29 09:12:20 -0700

    Stub AF::Fedora#init_base_path to avoid unnecessary calls to fcrepo during test

--- spec/requests/accounts_spec.rb
+++ spec/requests/accounts_spec.rb
@@ -1,6 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "Accounts", type: :request do
+  before do
+    allow_any_instance_of(ActiveFedora::Fedora).to receive(:init_base_path)
+  end
   describe "GET /accounts" do
     it "works! (now write some real specs)" do
       get accounts_path
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'Home page', type: :request do
+  before do
+    allow_any_instance_of(ActiveFedora::Fedora).to receive(:init_base_path)
+  end
+
   context 'without a current tenant' do
     describe 'GET /' do
       it 'redirects to the accounts landing page' do
