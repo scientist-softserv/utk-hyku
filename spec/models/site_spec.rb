37b7149096eddf05fef222280e0462f9c397bc92
aeschylus <scipioaffricanus@gmail.com>
2016-04-06 14:10:41 -0700

    Add Site model and scaffold for application settings and configuration

---
+++ spec/models/site_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe Site, type: :model do
+  describe ".instance" do
+    it "is a singleton site" do
+      expect(described_class.instance).to eq(described_class.instance)
+    end
+  end
+end
