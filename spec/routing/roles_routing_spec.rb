5fa4810d807b8ea2772bdddf2e512c34aa9bc85c
Chris Beer <cabeer@stanford.edu>
2016-04-08 11:59:09 -0700

    Add quick-and-dirty global roles editor

---
+++ spec/routing/roles_routing_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe RolesController, type: :routing do
+  describe "routing" do
+    it "routes to #edit" do
+      expect(get: "/site/roles").to route_to("roles#index")
+    end
+    it "routes to #update via PUT" do
+      expect(put: "/site/roles/1").to route_to("roles#update", id: "1")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/site/roles/1").to route_to("roles#update", id: "1")
+    end
+  end
+end
