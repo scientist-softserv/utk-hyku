f852a5de3f235d11630774b48a464058b0ef40d1
Chris Beer <cabeer@stanford.edu>
2016-04-11 11:20:54 -0700

    Add association between a Site and an Account

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,6 +1,9 @@
 class Site < ActiveRecord::Base
   validates :application_name, presence: true, allow_nil: true
 
+  belongs_to :account
+  accepts_nested_attributes_for :account, update_only: true
+
   def self.instance
     first || create
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408171411) do
+ActiveRecord::Schema.define(version: 20160411181343) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "tenant"
@@ -242,6 +242,7 @@ ActiveRecord::Schema.define(version: 20160408171411) do
     t.string   "application_name"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.integer  "account_id"
   end
 
   create_table "subject_local_authority_entries", force: :cascade do |t|
