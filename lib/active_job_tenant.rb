24dc838abe441b5d6e011c56d01c80fbba64261d
Chris Beer <cabeer@stanford.edu>
2016-04-18 16:43:00 -0700

    Extend ActiveJob::Base to support apartment tenants

---
+++ lib/active_job_tenant.rb
@@ -0,0 +1,46 @@
+module ActiveJobTenant
+  extend ActiveSupport::Concern
+
+  included do
+    attr_accessor :tenant
+
+    attr_writer :current_account
+  end
+
+  module ClassMethods
+    def deserialize(job_data)
+      super.tap do |job|
+        job.tenant = job_data['tenant']
+        job.current_account = nil
+      end
+    end
+  end
+
+  def serialize
+    super.merge('tenant' => Apartment::Tenant.current)
+  end
+
+  def perform_now
+    switch do
+      super
+    end
+  end
+
+  private
+
+    def current_account
+      @current_account ||= Account.find_by(tenant: current_tenant)
+    end
+
+    def current_tenant
+      tenant || Apartment::Tenant.current
+    end
+
+    def switch
+      Apartment::Tenant.switch(current_tenant) do
+        current_account.switch! if current_account
+
+        yield
+      end
+    end
+end
