db27ac2be7f4608baa5978b381e63bc6e74652c2
Chris Beer <cabeer@stanford.edu>
2016-04-19 07:22:33 -0700

    Move coverage analysis into rails_helper to improve coveralls reliability

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,16 +17,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-require 'simplecov'
-require 'coveralls'
-SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new(
-  [
-    SimpleCov::Formatter::HTMLFormatter,
-    Coveralls::SimpleCov::Formatter
-  ]
-)
-SimpleCov.start('rails')
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
