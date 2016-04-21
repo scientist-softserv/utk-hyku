974435a8e23bd47a4c8cf840544332672151aea7
Chris Beer <cabeer@stanford.edu>
2016-04-21 11:56:00 -0700

    Extract creating the fcrepo endpoint to a job, and set the fcrepo base path for each tenant

---
+++ spec/jobs/create_fcrepo_endpoint_job_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe CreateFcrepoEndpointJob do
+  let(:account) { FactoryGirl.create(:account) }
+
+  it 'sets the base path configuration for fcrepo' do
+    described_class.perform_now(account)
+
+    expect(account.fcrepo_endpoint.base_path).to eq "/#{account.tenant}"
+  end
+end
