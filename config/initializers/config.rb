ad588fde58add385a28561732bc59f0e67df0797
Chris Beer <cabeer@stanford.edu>
2016-04-28 16:01:36 -0700

    Allow config to be set through environment variables

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,6 +2,8 @@ Config.setup do |config|
   # Name of the constat exposing loaded settings
   config.const_name = 'Settings'
 
+  config.use_env = true
+
   # Ability to remove elements of the array set in earlier loaded settings file. Default: nil
   # config.knockout_prefix = '--'
 end
