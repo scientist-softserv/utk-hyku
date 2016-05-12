191ea06c4fea52a8834d71e6a240d0cc1d9ad863
Chris Beer <cabeer@stanford.edu>
2016-05-12 15:45:31 -0700

    Use active_elastic_job for workers in production

--- config/application.rb
+++ config/application.rb
@@ -39,7 +39,5 @@ module Lerna
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
-    config.active_job.queue_adapter = :sidekiq
   end
 end
