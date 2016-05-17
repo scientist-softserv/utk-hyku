5d10db005c1e960f124a72687d9228b135df3205
Michael J. Giarlo <mjgiarlo@stanford.edu>
2016-05-17 12:19:40 -0700

    Obey the rubocop

--- app/actors/curation_concerns/actors/generic_work_actor.rb
+++ app/actors/curation_concerns/actors/generic_work_actor.rb
@@ -1,6 +1,8 @@
 # Generated via
 #  `rails generate curation_concerns:work GenericWork`
-module CurationConcerns::Actors
-  class GenericWorkActor < CurationConcerns::Actors::BaseActor
+module CurationConcerns
+  module Actors
+    class GenericWorkActor < CurationConcerns::Actors::BaseActor
+    end
   end
 end
