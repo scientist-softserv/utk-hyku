a6a50fb142aaeee2efc343ad4c9ca9f562d43f59
Chris Beer <cabeer@stanford.edu>
2016-05-25 13:51:40 -0700

    Add comment about issue with railsconfig/config#44, and our current workaround

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,6 +3,14 @@ Config.setup do |config|
   config.const_name = 'Settings'
 
   config.use_env = true
+  
+  # Pending https://github.com/railsconfig/config/pull/144; until then, we're
+  # explicitly looking up environment settings on an as-needed basis in settings.yml
+  #
+  # config.env_prefix = 'SETTINGS'
+  # config.env_separator = '__'
+  # config.env_converter = :downcase
+  # config.env_parse_values = true
 
   # Ability to remove elements of the array set in earlier loaded settings file. Default: nil
   # config.knockout_prefix = '--'
