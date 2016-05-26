d2770eb5d21f35a956f01d4711b7517454915d02
Justin Coyne <jcoyne@justincoyne.com>
2016-05-25 17:14:00 -0700

    Only redirect to the splash page on the root uri.
    Other uris without account should be 404.

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -2,14 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Home page', type: :request do
   context 'without a current tenant' do
+    let(:conf) { double(enabled: true, host: 'localhost') }
     before do
-      allow(Settings).to receive(:multitenancy).and_return(double(enabled: true))
+      allow(Settings).to receive(:multitenancy).and_return(conf)
     end
 
     describe 'GET /' do
-      it 'redirects to the accounts landing page' do
-        get root_path
-        expect(response).to redirect_to(splash_path)
+      context "on the primary host" do
+        before { host! 'localhost' }
+        it 'redirects to the accounts landing page' do
+          get root_path
+          expect(response).to redirect_to(splash_path)
+        end
+      end
+
+      context "on a subhost" do
+        before { host! 'foo.bar.com' }
+        it 'raises a 404' do
+          expect { get root_path }.to raise_error(ActionController::RoutingError)
+        end
       end
     end
   end
