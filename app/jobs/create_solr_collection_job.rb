5c3d3a47362ef77825fba6d9b5d7660baa4483e5
Chris Beer <cabeer@stanford.edu>
2016-06-02 10:36:07 -0700

    Transform settings from nested hashes to solr parameter keys

--- app/jobs/create_solr_collection_job.rb
+++ app/jobs/create_solr_collection_job.rb
@@ -13,6 +13,43 @@ class CreateSolrCollectionJob < ActiveJob::Base
     account.update(solr_endpoint_attributes: { url: collection_url(name), collection: name })
   end
 
+  # Transform settings from nested, snaked-cased options to flattened, camel-cased options
+  class CollectionOptions
+    attr_reader :settings
+
+    def initialize(settings = {})
+      @settings = settings
+    end
+
+    ##
+    # @example Camel-casing
+    #   { replication_factor: 5 } # => { "replicationFactor" => 5 }
+    # @example Blank-rejecting
+    #   { emptyValue: '' } #=> { }
+    # @example Nested value-flattening
+    #   { collection: { config_name: 'x' } } # => { 'collection.configName' => 'x' }
+    def to_h
+      Hash[*settings.map { |k, v| transform_entry(k, v) }.flatten].reject { |_k, v| v.blank? }.symbolize_keys
+    end
+
+    private
+
+      def transform_entry(k, v)
+        case v
+        when Hash
+          v.map do |k1, v1|
+            ["#{transform_key(k)}.#{transform_key(k1)}", v1]
+          end
+        else
+          [transform_key(k), v]
+        end
+      end
+
+      def transform_key(k)
+        k.to_s.camelize(:lower)
+      end
+  end
+
   private
 
     def client
@@ -20,7 +57,7 @@ class CreateSolrCollectionJob < ActiveJob::Base
     end
 
     def collection_options
-      Settings.solr.collection_options.to_h.reject { |_k, v| v.blank? }
+      CollectionOptions.new(Settings.solr.collection_options.to_hash).to_h
     end
 
     def collection_exists?(name)
--- spec/jobs/create_solr_collection_job_spec.rb
+++ spec/jobs/create_solr_collection_job_spec.rb
@@ -31,4 +31,32 @@ RSpec.describe CreateSolrCollectionJob do
       described_class.perform_now(account)
     end
   end
+
+  describe CreateSolrCollectionJob::CollectionOptions do
+    describe '#to_h' do
+      subject { described_class.new(data).to_h }
+      let(:data) do
+        {
+          collection: { config_name: 'hybox', blank: '' },
+          num_shards: 1,
+          replication_factor: 5,
+          rule: 'asdf',
+          blank: ''
+        }
+      end
+
+      it 'removes blank values' do
+        expect(subject).not_to include(blank: '')
+        expect(subject).not_to include('collection.blank': '')
+      end
+
+      it 'collapses nested hashes' do
+        expect(subject).to include('collection.configName': 'hybox')
+      end
+
+      it 'camelizes key values' do
+        expect(subject).to include(replicationFactor: 5)
+      end
+    end
+  end
 end
