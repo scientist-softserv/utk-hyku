81a3b3bc3e250e6a33f5804be426d5560ce57c35
Justin Coyne <jcoyne@justincoyne.com>
2016-06-10 15:06:46 -0500

    Update dependencies

--- app/services/iiif_thumbnail_path_service.rb
+++ app/services/iiif_thumbnail_path_service.rb
@@ -1,25 +1,5 @@
 class IIIFThumbnailPathService < Sufia::WorkThumbnailPathService
   class << self
-    # TODO: we need to override this method until https://github.com/projecthydra/curation_concerns/pull/843 is merged
-    # @param [Work, FileSet] the object to get the thumbnail for
-    # @return [String] a path to the thumbnail
-    # rubocop:disable Metrics/MethodLength
-    def call(object)
-      return default_image unless object.thumbnail_id
-
-      thumb = fetch_thumbnail(object)
-      return unless thumb
-      return call(thumb) unless thumb.is_a?(::FileSet)
-      if thumb.audio?
-        audio_image
-      elsif thumbnail?(thumb)
-        thumbnail_path(thumb)
-      else
-        default_image
-      end
-    end
-    # rubocop:enable Metrics/MethodLength
-
     protected
 
       # @param [FileSet] file_set
