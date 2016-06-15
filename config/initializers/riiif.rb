fb4138d982c37e6975fa740de54d53cb66894358
Justin Coyne <jcoyne@justincoyne.com>
2016-06-15 09:18:14 -0500

    Authorize IIIF images
    Fixes #117
    
    404 image is from
    https://commons.wikimedia.org/wiki/File:US_404_(1961).svg
    and is in the public domain because it comes from the Manual on Uniform
    Traffic Control Devices, sign number M1-4, which states specifically on
    page I-1 that:
    
        Any traffic control device design or application provision contained
        in this Manual shall be considered to be in the public domain.
        Traffic control devices contained in this Manual shall not be
        protected by a patent, trademark, or copyright, except for the
        Interstate Shield and any other items owned by FHWA.

--- config/initializers/riiif.rb
+++ config/initializers/riiif.rb
@@ -23,5 +23,9 @@ Riiif::Image.file_resolver.id_to_uri = lambda do |id|
 end
 # Riiif::Image.file_resolver.basic_auth_credentials = [ActiveFedora.fedora.user, ActiveFedora.fedora.password]
 
+
+Riiif::Image.authorization_service = IIIFAuthorizationService
+
+Riiif.not_found_image = 'app/assets/images/us_404.svg'
 Riiif::Engine.config.cache_duration_in_days = 365
 
---
+++ spec/requests/riiif_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'IIIF image API' do
+  let(:user) { create(:user) }
+  let(:work) { create(:work_with_one_file, user: user) }
+  let(:file_set) { work.ordered_members.to_a.first }
+  before do
+    Hydra::Works::AddFileToFileSet.call(file_set,
+                                        fixture_file('images/world.png'),
+                                        :original_file)
+  end
+  let(:file) { file_set.original_file }
+  let(:size) { '300,' }
+
+  describe 'GET /images/{id}' do
+    context "when the user is authorized" do
+      it "returns an image" do
+        login_as user
+        get Riiif::Engine.routes.url_helpers.image_path(file.id, size: size)
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq 'image/jpeg'
+      end
+    end
+
+    context "when the user is not authorized" do
+      it "returns an image" do
+        get Riiif::Engine.routes.url_helpers.image_path(file.id, size: size)
+        expect(response).to have_http_status(:unauthorized)
+        expect(response.content_type).to eq 'image/jpeg'
+      end
+    end
+  end
+end
