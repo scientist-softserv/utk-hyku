0279099a730164807d9d94e2482e2f2d12cce057
Chris Beer <chris@cbeer.info>
2016-08-31 11:52:21 -0700

    Fix SQS job enqueueing problem
    Rails 5 updated the adapter API, and upstream has hard-coded their class name.

---
+++ spec/lib/active_job/queue_adapters/better_active_elastic_job_adapter_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+require 'active_job/queue_adapters/better_active_elastic_job_adapter'
+
+RSpec.describe ActiveJob::QueueAdapters::BetterActiveElasticJobAdapter do
+  let(:queue_url) { 'http://example.com/queue/url' }
+  let(:account) { create(:account) }
+  let(:aws_sqs_client) { instance_double(Aws::SQS::Client) }
+  let(:job) { CreateSolrCollectionJob.new(account) }
+
+  before do
+    allow(Settings.active_job_queue).to receive(:url).and_return(queue_url)
+    allow(described_class).to receive(:aws_sqs_client).and_return(aws_sqs_client)
+  end
+
+  describe '#enqueue' do
+    it 'uses the configured queue' do
+      expect(aws_sqs_client).to receive(:send_message) do |msg|
+        expect(msg[:queue_url]).to eq queue_url
+      end
+
+      subject.enqueue job
+    end
+  end
+end
