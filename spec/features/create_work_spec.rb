64438f91ce397fe51d641a3eca8e9b11084aa314
Justin Coyne <jcoyne@justincoyne.com>
2016-08-31 19:06:00 -0500

    Move the rights authority file to the license authority file (#313)

---
+++ spec/features/create_work_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.feature 'Creating a new Work' do
+  let(:user) { create(:user) }
+
+  before do
+    login_as user, scope: :user
+  end
+
+  it 'creates the work' do
+    visit '/'
+    click_link "Share Your Work"
+    expect(page).to have_content "Add New Work"
+  end
+end
