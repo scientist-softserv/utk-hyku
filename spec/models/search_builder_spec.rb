8eaa1683bb092b7cafaedb8efc5a157fb0e48b4d
Justin Coyne <jcoyne@justincoyne.com>
2016-09-01 11:13:57 -0500

    Searches should filter by model

---
+++ spec/models/search_builder_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe SearchBuilder do
+  describe ".default_processor_chain" do
+    subject { described_class.default_processor_chain }
+    it { is_expected.to include :filter_models }
+  end
+end
