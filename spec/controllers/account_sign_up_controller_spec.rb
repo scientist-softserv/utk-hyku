e9ce44a5a0c57d2b664c8130ec9cf4d5b6ad74f5
Chris Beer <chris@cbeer.info>
2016-09-12 15:21:13 -0700

    Extract new account sign up controller

---
+++ spec/controllers/account_sign_up_controller_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe AccountSignUpController, type: :controller do
+  let(:user) {}
+
+  before do
+    sign_in user if user
+  end
+
+  # This should return the minimal set of attributes required to create a valid
+  # Account. As you add validations to Account, be sure to
+  # adjust the attributes here as well.
+  let(:valid_attributes) do
+    { name: 'x' }
+  end
+
+  let(:invalid_attributes) do
+    { tenant: 'missing-cname', cname: '' }
+  end
+
+  context 'as an anonymous user' do
+    describe "GET #new" do
+      it "assigns a new account as @account" do
+        get :new
+        expect(response).to render_template("layouts/sufia-one-column")
+        expect(assigns(:account)).to be_a_new(Account)
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid params" do
+        before do
+          allow_any_instance_of(CreateAccount).to receive(:create_external_resources)
+        end
+
+        it "creates a new Account" do
+          expect do
+            post :create, params: { account: valid_attributes }
+          end.to change(Account, :count).by(1)
+        end
+      end
+
+      context "with invalid params" do
+        it "assigns a newly created but unsaved account as @account" do
+          post :create, params: { account: invalid_attributes }
+          expect(assigns(:account)).to be_a_new(Account)
+        end
+
+        it "re-renders the 'new' template" do
+          post :create, params: { account: invalid_attributes }
+          expect(response).to render_template("new")
+        end
+      end
+    end
+  end
+end
--- spec/features/splash_spec.rb
+++ spec/features/splash_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "The splash page" do
   it "shows the page" do
     visit '/splash'
-    expect(page).to have_link 'Get Started'
+    expect(page).to have_link 'Get Started', href: account_sign_up_path
   end
 end
