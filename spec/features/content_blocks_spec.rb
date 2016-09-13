52d35bf0ed348f2012fc8ebe3932c73026f51137
Chris Beer <chris@cbeer.info>
2016-09-13 14:24:31 -0700

    Split sites controller into labels + content_blocks

---
+++ spec/features/content_blocks_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Site content blocks configuration' do
+  context 'as an administrator' do
+    let(:user) { FactoryGirl.create(:admin) }
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    describe 'homepage content blocks' do
+      before do
+        Site.update(
+          announcement_text: 'FOO',
+          marketing_text: 'BAZ',
+          featured_researcher: 'QUUX'
+        )
+      end
+      it 'updates the homepage' do
+        visit root_path
+        expect(page).to have_content('FOO')
+        expect(page).to have_content('BAZ')
+        expect(page).to have_content('QUUX')
+      end
+    end
+  end
+end
---
+++ spec/routing/labels_routing_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe LabelsController, type: :routing do
+  describe "routing" do
+    it "routes to #edit" do
+      expect(get: "/site/labels/edit").to route_to("labels#edit")
+    end
+    it "routes to #update via PUT" do
+      expect(put: "/site/labels").to route_to("labels#update")
+    end
+
+    it "routes to #update via PATCH" do
+      expect(patch: "/site/labels").to route_to("labels#update")
+    end
+  end
+end
