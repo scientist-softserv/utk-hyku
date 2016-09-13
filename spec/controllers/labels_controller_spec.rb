71e154e315eddfe447b0314bbd07a6bc42b75926
Chris Beer <chris@cbeer.info>
2016-09-13 14:24:31 -0700

    Split sites controller into labels + content_blocks

--- spec/controllers/labels_controller_spec.rb
+++ spec/controllers/labels_controller_spec.rb
@@ -18,14 +18,11 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe SitesController, type: :controller do
+RSpec.describe LabelsController, type: :controller do
   before do
     sign_in user
   end
 
-  # This should return the minimal set of attributes required to create a valid
-  # Site. As you add validations to Site, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) do
     {
       application_name: "Custom Name",
@@ -38,24 +35,19 @@ RSpec.describe SitesController, type: :controller do
     { application_name: "" }
   end
 
-  # This should return the minimal set of values that should be in the session
-  # in order to pass any filters (e.g. authentication) defined in
-  # SitesController. Be sure to keep this updated too.
-  let(:valid_session) { {} }
-
   context 'with an unprivileged user' do
     let(:user) { FactoryGirl.create(:user) }
 
     describe "GET #edit" do
       it "denies the request" do
-        get :edit, {}
+        get :edit
         expect(response).to have_http_status(401)
       end
     end
 
     describe "PUT #update" do
       it "denies the request" do
-        put :update, {}
+        put :update
         expect(response).to have_http_status(401)
       end
     end
@@ -66,7 +58,7 @@ RSpec.describe SitesController, type: :controller do
 
     describe "GET #edit" do
       it "assigns the requested site as @site" do
-        get :edit, {}, valid_session
+        get :edit, params: {}
         expect(assigns(:site)).to eq(Site.instance)
       end
     end
@@ -82,65 +74,32 @@ RSpec.describe SitesController, type: :controller do
         end
 
         it "updates the requested site" do
-          put :update, { site: new_attributes }, valid_session
+          put :update, params: { site: new_attributes }
           Site.reload
           expect(Site.application_name).to eq "New Custom Name"
           expect(Site.institution_name).to eq "New Custom Inst Name"
           expect(Site.institution_name_full).to eq "New Full Custom Inst Name"
         end
 
-        context "with content blocks" do
-          let(:new_attributes) do
-            {
-              application_name: "New Custom Name",
-              institution_name: "New Custom Inst Name",
-              institution_name_full: "New Full Custom Inst Name",
-              announcement_text: 'This is announcement text!',
-              marketing_text: 'This is marketing text!',
-              featured_researcher: 'This is featured researcher!'
-            }
-          end
-
-          let!(:announcement) do
-            FactoryGirl.create(:content_block, name: 'announcement_text')
-          end
-
-          let!(:marketing) do
-            FactoryGirl.create(:content_block, name: 'marketing_text')
-          end
-
-          let!(:researcher) do
-            FactoryGirl.create(:content_block, name: 'featured_researcher')
-          end
-
-          it "updates the requested site" do
-            put :update, { site: new_attributes }, valid_session
-            Site.reload
-            expect(Site.announcement_text.value).to eq "This is announcement text!"
-            expect(Site.marketing_text.value).to eq "This is marketing text!"
-            expect(Site.featured_researcher.value).to eq "This is featured researcher!"
-          end
-        end
-
         it "assigns the requested site as @site" do
-          put :update, { site: valid_attributes }, valid_session
+          put :update, params: { site: valid_attributes }
           expect(assigns(:site)).to eq(Site.instance)
         end
 
         it "redirects to the site" do
-          put :update, { site: valid_attributes }, valid_session
-          expect(response).to redirect_to(edit_site_path)
+          put :update, params: { site: valid_attributes }
+          expect(response).to redirect_to(edit_site_labels_path)
         end
       end
 
       context "with invalid params" do
         it "assigns the site as @site" do
-          put :update, { site: invalid_attributes }, valid_session
+          put :update, params: { site: invalid_attributes }
           expect(assigns(:site)).to eq(Site.instance)
         end
 
         it "re-renders the 'edit' template" do
-          put :update, { site: invalid_attributes }, valid_session
+          put :update, params: { site: invalid_attributes }
           expect(response).to render_template("edit")
         end
       end
