871bd2c243f9d6873ed618c0f67cdee883545522
Justin Coyne <jcoyne@justincoyne.com>
2016-10-17 09:41:07 -0500

    Update to latest CurationConcerns and Sufia

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -2,7 +2,6 @@ class CatalogController < ApplicationController
   include Hydra::Catalog
   include Hydra::Controller::ControllerBehavior
   include Sufia::Catalog
-  include BlacklightAdvancedSearch::Controller
 
   # These before_filters apply the hydra access controls
   before_action :enforce_show_permissions, only: :show
