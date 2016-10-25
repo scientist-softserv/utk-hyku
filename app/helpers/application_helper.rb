a3526bf60afefe8f9cdcbe946df206f6ebb07706
Chris Beer <chris@cbeer.info>
2016-10-25 16:04:30 -0700

    Update the admin sidebar to use standard bootstrap components

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
   include ::SufiaHelper
   include ContentBlockHelper
+
+  def nav_link(href)
+    content_tag(:li, class: ('active' if current_page?(href))) do
+      link_to(href) do
+        yield
+      end
+    end
+  end
 end
