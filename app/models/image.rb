5bde940438071f4e215217c6a9f16e95281728f7
Justin Coyne <jcoyne@justincoyne.com>
2016-10-25 10:57:42 -0500

    Add a second work type (Image)

---
+++ app/models/image.rb
@@ -0,0 +1,11 @@
+# Generated via
+#  `rails generate curation_concerns:work Image`
+class Image < ActiveFedora::Base
+  include ::CurationConcerns::WorkBehavior
+  include ::CurationConcerns::BasicMetadata
+  include Sufia::WorkBehavior
+  self.human_readable_type = 'Image'
+  # Change this to restrict which works can be added as a child.
+  # self.valid_child_concerns = []
+  validates :title, presence: { message: 'Your work must have a title.' }
+end
