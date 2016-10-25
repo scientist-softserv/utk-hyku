df3861fce68bbdb33ef959d98d23da692764c07e
Justin Coyne <jcoyne@justincoyne.com>
2016-10-25 10:57:42 -0500

    Add a second work type (Image)

---
+++ spec/controllers/curation_concerns/images_controller_spec.rb
@@ -0,0 +1,9 @@
+# Generated via
+#  `rails generate curation_concerns:work Image`
+require 'rails_helper'
+
+RSpec.describe CurationConcerns::ImagesController do
+  it "has tests" do
+    skip "Add your tests here"
+  end
+end
