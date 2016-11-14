f1d78b80de58dc1a8a39867fdba1fe67d7a8541f
Justin Coyne <jcoyne@justincoyne.com>
2016-11-14 13:39:09 -0600

    Revert "Add Redis sentinel clustering configuration"
    This reverts commit 2473aab7ef03a529d43ca20a8e33098f2a3dd354.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,9 @@
 config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
-redis_conn = proc {
-  Redis.new url: "redis://#{config[:host]}:#{config[:port]}/", role: :master, sentinels: [(config[:sentinel] if config[:sentinel] && config[:sentinel][:host])]
-}
-
 Sidekiq.configure_server do |s|
-  s.redis = ConnectionPool.new(&redis_conn)
+  s.redis = { url: "redis://#{config[:host]}:#{config[:port]}/" }
 end
 
 Sidekiq.configure_client do |s|
-  s.redis = ConnectionPool.new(&redis_conn)
+  s.redis = { url: "redis://#{config[:host]}:#{config[:port]}/" }
 end
