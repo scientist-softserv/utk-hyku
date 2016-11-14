e35ebc340d2564a4d20d84f6c2d1ad8197929083
Justin Coyne <jcoyne@justincoyne.com>
2016-11-14 16:58:42 -0600

    Only use a ConnectionPool if the Redis cluster is enabled
    This enables Redis clustering to be disabled for AWS, which works around
    https://github.com/projecthydra-labs/hyku/issues/466

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,13 +1,21 @@
 config = YAML.load(ERB.new(IO.read(Rails.root + 'config' + 'redis.yml')).result)[Rails.env].with_indifferent_access
 
-redis_conn = proc {
-  Redis.new url: "redis://#{config[:host]}:#{config[:port]}/", role: :master, sentinels: [(config[:sentinel] if config[:sentinel] && config[:sentinel][:host])]
-}
+redis_conn = if ENV['DISABLE_REDIS_CLUSTER']
+               # Clustering must be disabled on AWS because it breaks Apartment switching.
+               # See https://github.com/projecthydra-labs/hyku/issues/430
+               proc { { url: "redis://#{config[:host]}:#{config[:port]}/" } }
+             else
+               proc {
+                 ConnectionPool.new do
+                   Redis.new url: "redis://#{config[:host]}:#{config[:port]}/", role: :master, sentinels: [(config[:sentinel] if config[:sentinel] && config[:sentinel][:host])]
+                 end
+               }
+             end
 
 Sidekiq.configure_server do |s|
-  s.redis = ConnectionPool.new(&redis_conn)
+  s.redis = redis_conn.call
 end
 
 Sidekiq.configure_client do |s|
-  s.redis = ConnectionPool.new(&redis_conn)
+  s.redis = redis_conn.call
 end
