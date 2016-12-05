e4c9986c9c51213bfd8d343e5a34cb6747bb6b92
Justin Coyne <jcoyne@justincoyne.com>
2016-12-05 10:07:12 -0600

    Keep the settings section expanded when we're in one of its controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,4 @@
 module ApplicationHelper
   include ::HyraxHelper
   include ContentBlockHelper
-
-  def nav_link(href)
-    content_tag(:li, class: ('active' if current_page?(href))) do
-      link_to(href) do
-        yield
-      end
-    end
-  end
 end
