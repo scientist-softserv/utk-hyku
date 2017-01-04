aa7e0d1dbad954256ce1e6ef2e2674ba68440562
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-04 15:05:47 -0500

    Adding filter user list controls

--- app/controllers/admin/group_users_controller.rb
+++ app/controllers/admin/group_users_controller.rb
@@ -4,7 +4,7 @@ module Admin
 
     def index
       @group = Hyku::Group.find_by_id(params[:group_id])
-      @users = Array.wrap(User.first)
+      @users = User.page(page_number).per(page_size)
       render template: 'admin/groups/users'
     end
 
@@ -13,5 +13,14 @@ module Admin
 
     def remove
     end
+
+    private
+      def page_number
+        params[:page].to_i || 1
+      end
+
+      def page_size
+        params[:per].to_i || 10
+      end
   end
 end
