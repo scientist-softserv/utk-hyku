05a49bfa2f5a3f827c233060bc2e4ff7166f476f
Justin Coyne <jcoyne@justincoyne.com>
2017-01-05 15:50:32 -0600

    Fix asset loading for tinymce-rails-imageupload
    Fixes #582

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,11 @@ module Hyku
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+
+    # The compile method (default in tinymce-rails 4.5.2) doesn't work when also
+    # using tinymce-rails-imageupload, so revert to the :copy method
+    # https://github.com/spohlenz/tinymce-rails/issues/183
+    config.tinymce.install = :copy
   end
 end
