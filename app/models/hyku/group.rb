c7601063ce04fd1bd474ef0f677f3299b98b199a
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-05 11:14:08 -0500

    Allowing group membership to be revoked from the UI

--- app/models/hyku/group.rb
+++ app/models/hyku/group.rb
@@ -6,14 +6,14 @@ module Hyku
     MEMBERSHIP_ROLE = :member
     DEFAULT_MEMBER_CLASS = User
 
-    def self.add_members_by_id(ids:, group:, member_class: DEFAULT_MEMBER_CLASS)
-      new_member_ids = Array.wrap(ids)
-      new_members = member_class.find(new_member_ids)
-      new_members.collect { |m| m.add_role MEMBERSHIP_ROLE, group }
+    def add_members_by_id(ids, member_class: DEFAULT_MEMBER_CLASS)
+      new_members = member_class.find(Array.wrap(ids))
+      new_members.collect { |m| m.add_role(MEMBERSHIP_ROLE, self) }
     end
 
-    def add_members_by_id(ids, member_class: DEFAULT_MEMBER_CLASS)
-      self.class.add_members_by_id(ids: ids, group: self, member_class: member_class)
+    def remove_members_by_id(ids, member_class: DEFAULT_MEMBER_CLASS)
+      old_members = member_class.find(Array.wrap(ids))
+      old_members.collect { |m| m.remove_role(MEMBERSHIP_ROLE, self) }
     end
 
     def members(member_class: DEFAULT_MEMBER_CLASS)
