f76cc59915837d573dee1be51ec3953777b8a01d
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-10 16:16:03 -0500

    Tests for GroupsController
    Fixed update action and simplified branching

--- spec/controllers/admin/groups_controller_spec.rb
+++ spec/controllers/admin/groups_controller_spec.rb
@@ -3,10 +3,8 @@ require 'rails_helper'
 RSpec.describe Admin::GroupsController do
   context 'as an anonymous user' do
     describe 'GET #index' do
-      it 'is unauthorized' do
-        get :index
-        expect(response).to redirect_to root_path
-      end
+      subject { get :index }
+      it { is_expected.to redirect_to root_path }
     end
   end
 
@@ -14,28 +12,53 @@ RSpec.describe Admin::GroupsController do
     before { sign_in create(:admin) }
 
     describe 'GET #index' do
-      it 'uses the admin layout' do
-        get :index
-        expect(response).to render_template('layouts/admin')
-      end
+      subject { get :index }
+      it { is_expected.to render_template('layouts/admin') }
+      it { is_expected.to render_template('admin/groups/index') }
     end
 
     describe 'GET #new' do
+      subject { get :new }
+      it { is_expected.to render_template('admin/groups/new') }
     end
 
     describe 'POST #create' do
+      it 'creates a group when it recieves valid attribtes' do
+        expect do
+          post :create, params: { hyku_group: FactoryGirl.attributes_for(:group) }
+        end.to change(Hyku::Group, :count).by(1)
+      end
     end
 
-    describe 'GET #edit' do
-    end
+    context 'with an existing group' do
+      let(:group) { FactoryGirl.create(:group) }
+      let(:new_attributes) { FactoryGirl.attributes_for(:group) }
 
-    describe 'PUT #update' do
-    end
+      describe 'GET #edit' do
+        subject { get :edit, params: { id: group.id } }
+        it { is_expected.to render_template('admin/groups/edit') }
+      end
 
-    describe 'GET #remove' do
-    end
+      describe 'PATCH #update' do
+        subject { Hyku::Group.find_by_id(group.id) }
+        before { patch :update, params: { id: group.id, hyku_group: new_attributes } }
 
-    describe 'DELETE #destroy' do
+        it 'updates attribtes' do
+          expect(subject.name).to eq(new_attributes.fetch(:name))
+          expect(subject.description).to eq(new_attributes.fetch(:description))
+        end
+      end
+
+      describe 'GET #remove' do
+        subject { get :remove, params: { id: group.id } }
+        it { is_expected.to render_template('admin/groups/remove') }
+      end
+
+      describe 'DELETE #destroy' do
+        subject { Hyku::Group.find_by_id(group.id) }
+        before { delete :destroy, params: { id: group.id } }
+        it { is_expected.to be_nil }
+      end
     end
   end
 end
