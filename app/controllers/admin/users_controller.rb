26f0eec15c30954422ad9d9b7eb03ff212683643
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-12 15:21:42 -0500

    Fixing regression failure in Admin::UsersController
    .deny_access is defined in Hyrax::UsersControllerBehavior and thus
    overrides the .deny_access in AdminController. Re-defining it again
    seems a bit silly but it works.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,5 +5,11 @@ module Admin
     def self.local_prefixes
       ['hyrax/users']
     end
+
+    private
+
+      def deny_access(_exception)
+        redirect_to main_app.root_url, alert: 'You are not authorized to view this page'
+      end
   end
 end
