fcec9ff56d7fc4a2dd18a6124f04d7cbf931bd9f
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-18 13:26:18 -0500

    Test for the appropriate date format

--- spec/views/admin/groups/index.html.erb_spec.rb
+++ spec/views/admin/groups/index.html.erb_spec.rb
@@ -22,11 +22,11 @@ RSpec.describe 'admin/groups/index', type: :view do
     it 'renders a list of accounts' do
       expect(rendered).to have_selector('td', text: group_1.name)
       expect(rendered).to have_selector('td', text: group_1.number_of_users)
-      expect(rendered).to have_selector('td', text: group_1.created_at.to_date.to_s(:db))
+      expect(rendered).to have_selector('td', text: group_1.created_at.to_date.to_formatted_s(:standard))
 
       expect(rendered).to have_selector('td', text: group_2.name)
       expect(rendered).to have_selector('td', text: group_2.number_of_users)
-      expect(rendered).to have_selector('td', text: group_2.created_at.to_date.to_s(:db))
+      expect(rendered).to have_selector('td', text: group_2.created_at.to_date.to_formatted_s(:standard))
     end
 
     it 'has a button to create a new group' do
