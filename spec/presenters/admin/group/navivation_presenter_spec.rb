c1e7494c3a938cf594026f7c0f27f72099d49dbd
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-01-18 16:15:31 -0500

    Tests for group navigation presenter

---
+++ spec/presenters/admin/group/navivation_presenter_spec.rb
@@ -0,0 +1,56 @@
+require 'rails_helper'
+
+RSpec.describe Hyku::Admin::Group::NavigationPresenter do
+  let(:active_css_class) { described_class::Tab::ACTIVE_CSS_CLASS }
+  let(:base_params) do
+    {
+      controller: 'admin/groups',
+      id: 1
+    }
+  end
+
+  context 'edit page' do
+    let(:action) { 'edit' }
+    let(:params) { base_params.merge(action: action) }
+    let(:presenter) { described_class.new(params: params) }
+    subject { presenter.tabs }
+
+    it 'has 3 tabs' do
+      expect(subject.count).to be(3)
+    end
+
+    it 'has the edit tab marked as active' do
+      expect(subject.select { |tab| tab.css_class == active_css_class && tab.action == action }.count).to be(1)
+    end
+  end
+
+  context 'members page' do
+    let(:action) { 'index' }
+    let(:params) { base_params.merge(controller: 'admin/group_users', action: action) }
+    let(:presenter) { described_class.new(params: params) }
+    subject { presenter.tabs }
+
+    it 'has 3 tabs' do
+      expect(subject.count).to be(3)
+    end
+
+    it 'has the members tab marked as active' do
+      expect(subject.select { |tab| tab.css_class == active_css_class && tab.action == action }.count).to be(1)
+    end
+  end
+
+  context 'remve page' do
+    let(:action) { 'remove' }
+    let(:params) { base_params.merge(action: action) }
+    let(:presenter) { described_class.new(params: params) }
+    subject { presenter.tabs }
+
+    it 'has 3 tabs' do
+      expect(subject.count).to be(3)
+    end
+
+    it 'has the members tab marked as active' do
+      expect(subject.select { |tab| tab.css_class == active_css_class && tab.action == action }.count).to be(1)
+    end
+  end
+end
