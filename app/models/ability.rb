fb7e6c79a9579570f404bd51b4e07b6226584604
Chris Beer <chris@cbeer.info>
2017-02-15 09:46:37 -0800

    Remove unnecessary ability override now that hyrax doesn't grant overly broad permissions

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -15,10 +15,14 @@ class Ability
 
   def admin_permissions
     return unless admin?
+    return if superadmin?
+
     super
     can [:manage], [Site, Role, User]
 
-    restrict_site_admin_permissions unless current_user.has_role? :superadmin
+    can [:read, :update], Account do |account|
+      account == Site.account
+    end
   end
 
   def group_permissions
@@ -28,24 +32,13 @@ class Ability
   end
 
   def superadmin_permissions
-    return unless current_user.has_role? :superadmin
+    return unless superadmin?
 
     can :manage, :all
     can :peek, Hyku::Application
   end
 
-  def restrict_site_admin_permissions
-    # override curation_concerns admin roles to disable admin privileges on global models
-    cannot [:manage, :create, :discover, :show, :read, :edit, :update, :destroy], global_models
-
-    can [:read, :update], Account do |account|
-      account == Site.account
-    end
+  def superadmin?
+    current_user.has_role? :superadmin
   end
-
-  private
-
-    def global_models
-      [Account]
-    end
 end
