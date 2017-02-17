24d20a97957eacd010e05607c42d8e2b46a17e44
Justin Coyne <jcoyne@justincoyne.com>
2017-02-17 15:05:03 -0600

    Don't set storage = :fog until that adapter has been loaded
    See https://github.com/carrierwaveuploader/carrierwave/pull/1201/files

--- config/initializers/carrierwave_config.rb
+++ config/initializers/carrierwave_config.rb
@@ -3,11 +3,13 @@ require 'carrierwave'
 if Settings.s3.upload_bucket
   CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
-    config.storage = :fog
     config.fog_credentials = {
       provider: 'AWS',
       use_iam_profile: true
     }
+    # This must come after fog_credentials due to
+    # https://github.com/carrierwaveuploader/carrierwave/pull/1201/files
+    config.storage = :fog
     config.fog_directory = Settings.s3.upload_bucket
     config.fog_public = false
   end
