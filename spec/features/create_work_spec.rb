ee42e81622aa43519f7b6efecb7bbdc1541a7903
Justin Coyne <jcoyne@justincoyne.com>
2017-04-05 18:37:24 -0500

    Update to latest version of Hyrax

--- spec/features/create_work_spec.rb
+++ spec/features/create_work_spec.rb
@@ -1,7 +1,8 @@
-RSpec.feature 'Creating a new Work' do
+RSpec.feature 'Creating a new Work', :clean do
   let(:user) { create(:user) }
 
   before do
+    AdminSet.find_or_create_default_admin_set_id
     login_as user, scope: :user
   end
 
