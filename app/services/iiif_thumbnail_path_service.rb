48b8d17337a2d350c2e39cd4901a7257cac88a01
Justin Coyne <jcoyne@justincoyne.com>
2017-04-07 19:22:58 -0500

    Use a more appropriate thumbnail size
    This is closer to the size we are actually displaying it at.

--- app/services/iiif_thumbnail_path_service.rb
+++ app/services/iiif_thumbnail_path_service.rb
@@ -3,8 +3,10 @@ class IIIFThumbnailPathService < Hyrax::WorkThumbnailPathService
     protected
 
       # @param [FileSet] file_set
+      # @param [String] size ('!150,300') an IIIF image size defaults to an image no
+      #                      wider than 150px and no taller than 300px
       # @return the IIIF url for the thumbnail.
-      def thumbnail_path(file_set, size = '300,'.freeze)
+      def thumbnail_path(file_set, size = '!150,300'.freeze)
         file = file_set.original_file
         return unless file
         Riiif::Engine.routes.url_helpers.image_path(
