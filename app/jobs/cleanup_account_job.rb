f9328eb43adc009efcf33daef9b152ff537f97fc
tdonohue <tdonohue@duraspace.org>
2017-04-29 07:58:57 +1000

    Enhance CleanupAccountJob to also cleanup Redis namespace

--- app/jobs/cleanup_account_job.rb
+++ app/jobs/cleanup_account_job.rb
@@ -1,7 +1,13 @@
 class CleanupAccountJob < ActiveJob::Base
   def perform(account)
+    # Solr endpoint pulls its connection info from account directly
     cleanup_solr(account)
-    cleanup_fedora(account)
+    # Ensure Fedora endpoint's connection established to this account
+    # and ensure Redis::Namespace limited to this account's namespace
+    account.switch do
+      cleanup_fedora(account)
+      cleanup_redis(account)
+    end
     Apartment::Tenant.drop(account.tenant)
     account.destroy
   end
@@ -9,10 +15,25 @@ class CleanupAccountJob < ActiveJob::Base
   private
 
     def cleanup_fedora(account)
-      account.switch do
-        # Preceding slash must be removed from base_path when calling delete()
-        fcrepo_client.delete(account.fcrepo_endpoint.base_path.sub!(%r{^\/}, ''))
-      end
+      # Return immediately if fcrepo_endpoint doesn't exist
+      return unless account.fcrepo_endpoint
+      # Preceding slash must be removed from base_path when calling delete()
+      fcrepo_client.delete(account.fcrepo_endpoint.base_path.sub!(%r{^/}, ''))
+      account.fcrepo_endpoint.destroy
+    end
+
+    def cleanup_redis(account)
+      # Return immediately if redis_endpoint doesn't exist
+      return unless account.redis_endpoint
+      # Redis::Namespace currently doesn't support flushall or flushdb.
+      # See https://github.com/resque/redis-namespace/issues/56
+      # So, instead we select all keys in current namespace and delete
+      keys = redis_namespace.keys '*'
+      return if keys.empty?
+      # Delete in slices to avoid "stack level too deep" errors for large numbers of keys
+      # See https://github.com/redis/redis-rb/issues/122
+      keys.each_slice(1000) { |key_slice| redis_namespace.del(*key_slice) }
+      account.redis_endpoint.destroy
     end
 
     def cleanup_solr(account)
@@ -25,5 +46,10 @@ class CleanupAccountJob < ActiveJob::Base
 
     def fcrepo_client
       ActiveFedora.fedora.connection
+    end
+
+    def redis_namespace
+      # This is a Redis::Namespace which is already limited to account's namespace
+      Hyrax::RedisEventStore.instance
     end
 end
