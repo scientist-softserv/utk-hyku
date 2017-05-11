0d9c991ea6a8a813da3c03b5577571e382029cf0
Daniel Pierce <dlpierce@indiana.edu>
2017-05-11 17:08:49 -0400

    Restores indexing of descriptive metadata.

--- app/indexers/work_indexer.rb
+++ app/indexers/work_indexer.rb
@@ -1,3 +1,4 @@
 class WorkIndexer < Hyrax::WorkIndexer
+  include Hyrax::IndexesBasicMetadata
   self.thumbnail_path_service = IIIFThumbnailPathService
 end
--- spec/indexers/work_indexer_spec.rb
+++ spec/indexers/work_indexer_spec.rb
@@ -3,4 +3,8 @@ RSpec.describe WorkIndexer do
     subject { described_class.thumbnail_path_service }
     it { is_expected.to eq IIIFThumbnailPathService }
   end
+  describe 'rdf_service' do
+    subject { described_class.new(FactoryGirl.build(:work)).rdf_service }
+    it { is_expected.to eq Hyrax::BasicMetadataIndexer }
+  end
 end
