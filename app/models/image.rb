9ef141a6a5fcb73941de3fa2acc26db15e92bb24
Justin Coyne <jcoyne@justincoyne.com>
2017-05-12 14:31:46 -0500

    Linked data indexing for Image#based_near

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,11 +1,14 @@
 class Image < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
-  include ::Hyrax::BasicMetadata
   validates :title, presence: { message: 'Your work must have a title.' }
 
   property :extent, predicate: ::RDF::Vocab::DC.extent, multiple: true do |index|
     index.as :stored_searchable
   end
+
+  # This must come after the custom properties because it finalizes the metadata
+  # schema (by adding accepts_nested_attributes)
+  include ::Hyrax::BasicMetadata
 
   # This indexer uses IIIF thumbnails:
   self.indexer = WorkIndexer
--- spec/indexers/work_indexer_spec.rb
+++ spec/indexers/work_indexer_spec.rb
@@ -3,8 +3,13 @@ RSpec.describe WorkIndexer do
     subject { described_class.thumbnail_path_service }
     it { is_expected.to eq IIIFThumbnailPathService }
   end
+
   describe 'rdf_service' do
-    subject { described_class.new(FactoryGirl.build(:work)).rdf_service }
-    it { is_expected.to eq Hyrax::BasicMetadataIndexer }
+    subject { indexer.rdf_service }
+
+    let(:work) { instance_double(Image) }
+    let(:indexer) { described_class.new(work) }
+
+    it { is_expected.to eq Hyrax::DeepIndexingService }
   end
 end
