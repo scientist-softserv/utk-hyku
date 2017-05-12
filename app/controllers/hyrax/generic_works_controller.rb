74f12c3cf592a437b827795cb961ad205154eb9a
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-05-12 08:28:35 -0700

    hyrax controllers for works are missing Hyrax::BreadcrumbsForWorks

--- app/controllers/hyrax/generic_works_controller.rb
+++ app/controllers/hyrax/generic_works_controller.rb
@@ -2,6 +2,7 @@ module Hyrax
   class GenericWorksController < ApplicationController
     # Adds Hyrax behaviors to the controller.
     include Hyrax::WorksControllerBehavior
+    include Hyrax::BreadcrumbsForWorks
 
     self.curation_concern_type = GenericWork
 
