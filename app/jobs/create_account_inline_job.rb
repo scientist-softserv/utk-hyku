6c89c6612e22eafb1d431e0eaab3466415a5b405
Joe Atzberger <ohiocore@gmail.com>
2017-05-12 20:33:01 -0700

    Account creation job renovation
    - Remove Jobs that have no reason to be async
    - Put inline stuff inline, not behind unnecessary one-line jobs
    - Use new Hyrax::ApplicationJob as the injection point for
      ActiveJobTenant, leaving ActiveJob::Base available *without* switching
    - Update tests to match

--- app/jobs/create_account_inline_job.rb
+++
@@ -1,8 +0,0 @@
-class CreateAccountInlineJob < ActiveJob::Base
-  def perform(account)
-    CreateSolrCollectionJob.perform_now(account)
-    CreateFcrepoEndpointJob.perform_now(account)
-    CreateRedisNamespaceJob.perform_now(account)
-    # CreateDefaultAdminSetJob.perform_now(account) # handled in Apartment callback
-  end
-end
