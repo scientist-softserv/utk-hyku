70e4ebed69c7cb3f3efeed4187557d8bf8faf463
Chris Beer <chris@cbeer.info>
2017-05-16 10:55:59 -0700

    Revert "Turn off ParseError handling"
    This reverts commit 3fbdff91dd4eb285a62f4c99a3f4a381570a43b5.

--- config/application.rb
+++ config/application.rb
@@ -27,10 +27,6 @@ module Hyku
       "I18n::InvalidLocale" => :not_found
     )
 
-    # Rails handles ParseError by default, but it is occurring on SQS and I need more details about why,
-    # so turn it off.
-    ActionDispatch::ExceptionWrapper.rescue_responses.delete("ActionDispatch::ParamsParser::ParseError")
-
     if defined? ActiveElasticJob
       Rails.application.configure do
         config.active_elastic_job.process_jobs = Settings.worker == 'true'
