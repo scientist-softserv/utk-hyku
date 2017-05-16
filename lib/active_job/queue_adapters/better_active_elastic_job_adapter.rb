6646cd47c59055f6af27cb2836a6cfceb2647edb
Chris Beer <chris@cbeer.info>
2017-05-16 10:55:52 -0700

    Add note about our active-elastic-job monkey-patch

--- lib/active_job/queue_adapters/better_active_elastic_job_adapter.rb
+++ lib/active_job/queue_adapters/better_active_elastic_job_adapter.rb
@@ -6,6 +6,8 @@ module ActiveJob
       class << self
         private
 
+          # Upstream dynamically calculated queue urls for each job; we'd rather
+          # route jobs into a pre-determined queue url instead.
           def queue_url(*_)
             if Settings.active_job_queue.url
               Settings.active_job_queue.url
