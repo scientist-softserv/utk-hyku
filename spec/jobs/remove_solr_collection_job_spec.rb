d58a760178e6fb4cb31c2c6c328cfa99ddc1b7fb
tdonohue <tdonohue@duraspace.org>
2017-05-22 21:48:39 +0000

    Fix ability to destroy Solr endpoint's collection

--- spec/jobs/remove_solr_collection_job_spec.rb
+++ spec/jobs/remove_solr_collection_job_spec.rb
@@ -3,6 +3,11 @@ RSpec.describe RemoveSolrCollectionJob do
   let(:connection_options) { double }
   let(:connection) { double }
 
+  before do
+    # Stub connection_options to respond with itself when without('adapter') called
+    allow(connection_options).to receive(:without).with('adapter').and_return(connection_options)
+  end
+
   it 'destroys the solr collection' do
     expect(RSolr).to receive(:connect).with(connection_options).and_return(connection)
     expect(connection).to receive(:get).with('/solr/admin/collections',
