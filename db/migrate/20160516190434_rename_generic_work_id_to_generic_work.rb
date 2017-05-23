d3382773ce1204c917dab212c8e991830fafd805
Justin Coyne <jcoyne@justincoyne.com>
2017-05-23 12:05:26 -0500

    Rails 5.1 requires versioned migrations

--- db/migrate/20160516190434_rename_generic_work_id_to_generic_work.rb
+++ db/migrate/20160516190434_rename_generic_work_id_to_generic_work.rb
@@ -1,4 +1,4 @@
-class RenameGenericWorkIdToGenericWork < ActiveRecord::Migration
+class RenameGenericWorkIdToGenericWork < ActiveRecord::Migration[4.2]
   def change
     rename_column :featured_works, :generic_work_id, :work_id if column_exists?(:featured_works, :generic_work_id)
     rename_column :proxy_deposit_requests, :generic_work_id, :work_id if column_exists?(:proxy_deposit_requests, :generic_work_id)
