9df56de21a228b049bcb2bdf783a58fafe9a7fba
Justin Coyne <jcoyne@justincoyne.com>
2017-05-24 09:35:15 -0500

    Add a test for the collection model

--- spec/models/collection_spec.rb
+++ spec/models/collection_spec.rb
@@ -1,7 +1,7 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe Collection do
-  it "has tests" do
-    skip "Add your tests here"
+  it "is a hyrax collection" do
+    expect(described_class.ancestors).to include Hyrax::CollectionBehavior
   end
 end
