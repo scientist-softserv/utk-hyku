99f02911bd2ae5f2907a1078acb24993d37f3dc0
Justin Coyne <jcoyne@justincoyne.com>
2017-06-15 09:57:09 -0500

    Add a rake task to create a superadmin
    Fixes #1242

--- lib/tasks/grant_superadmin.rake
+++ lib/tasks/grant_superadmin.rake
@@ -1,13 +1,15 @@
-namespace :superadmin do
-  desc 'Grant the superadmin role to specified users'
-  task 'grant', [:user_list] => [:environment] do |_cmd, args|
-    raise ArgumentError, 'A list of users is required: `rake superadmin:grant[user1,user2,...]`' unless args.user_list
-    args.to_a.each do |u|
-      user = User.find_by_user_key(u)
-      if user
-        user.add_role(:superadmin)
-      else
-        warn("Could not find user #{u}")
+namespace :hyku do
+  namespace :superadmin do
+    desc 'Grant the superadmin role to specified users'
+    task 'grant', [:user_list] => [:environment] do |_cmd, args|
+      raise ArgumentError, 'A list of users is required: `rake superadmin:grant[user1,user2,...]`' unless args.user_list
+      args.to_a.each do |u|
+        user = User.find_by_user_key(u)
+        if user
+          user.add_role(:superadmin)
+        else
+          warn("Could not find user #{u}")
+        end
       end
     end
   end
--- spec/tasks/rake_spec.rb
+++ spec/tasks/rake_spec.rb
@@ -12,21 +12,21 @@ RSpec.describe "Rake tasks" do
     end
 
     it 'requires user_list argument' do
-      expect { run_task('superadmin:grant') }.to raise_error(ArgumentError)
+      expect { run_task('hyku:superadmin:grant') }.to raise_error(ArgumentError)
     end
 
     it 'warns when a user is not found' do
-      expect(run_task('superadmin:grant', 'missing@example.org')).to include 'Could not find user'
+      expect(run_task('hyku:superadmin:grant', 'missing@example.org')).to include 'Could not find user'
     end
 
     it 'grants a single user the superadmin role' do
-      run_task('superadmin:grant', user1.email)
+      run_task('hyku:superadmin:grant', user1.email)
       expect(user1.has_role?(:superadmin)).to eq true
       expect(user2.has_role?(:superadmin)).to eq false
     end
 
     it 'grants a multiple users the superadmin role' do
-      run_task('superadmin:grant', user1.email, user2.email)
+      run_task('hyku:superadmin:grant', user1.email, user2.email)
       expect(user1.has_role?(:superadmin)).to eq true
       expect(user2.has_role?(:superadmin)).to eq true
     end
