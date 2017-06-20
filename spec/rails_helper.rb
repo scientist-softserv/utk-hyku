befdc6e208cdddb63361bd56d208fd0d33c858b7
Justin Coyne <jcoyne@justincoyne.com>
2017-06-20 11:34:47 -0500

    Switch from Poltergeist to headless Chrome
    Poltergeist is no longer maintained
    Rails 5.1 introduces ActionDispatch::SystemTestCase which should allow
    our feature tests to be speed up when this is done:
    https://github.com/rspec/rspec-rails/pull/1813

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,7 +17,6 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
-require 'capybara/poltergeist'
 require 'database_cleaner'
 require 'active_fedora/cleaner'
 
@@ -54,23 +53,20 @@ ActiveRecord::Migration.maintain_test_schema!
 
 # Uses faster rack_test driver when JavaScript support not needed
 Capybara.default_driver = :rack_test
-Capybara.javascript_driver = :poltergeist
 
-# Adding the below to deal with random Capybara-related timeouts in CI.
-# Found in this thread: https://github.com/teampoltergeist/poltergeist/issues/375
-poltergeist_options = {
-  js_errors: false,
-  timeout: 30,
-  logger: nil,
-  phantomjs_logger: StringIO.new,
-  phantomjs_options: [
-    '--load-images=no',
-    '--ignore-ssl-errors=yes'
-  ]
-}
-Capybara.register_driver(:poltergeist) do |app|
-  Capybara::Poltergeist::Driver.new(app, poltergeist_options)
+Capybara.register_driver :headless_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
+    chromeOptions: {
+      args: %w[headless disable-gpu]
+    }
+  )
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    desired_capabilities: capabilities
+  )
 end
+Capybara.javascript_driver = :headless_chrome
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
