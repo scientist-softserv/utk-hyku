b4ad1b82a8ff0edd5edc161fc126102246a2185c
Michael J. Giarlo <mjgiarlo@stanford.edu>
2017-06-21 13:26:43 -0700

    Override Hyrax mailer to inject tenant name into contact form submissions
    Fixes #1230

---
+++ app/mailers/hyrax/contact_mailer.rb
@@ -0,0 +1,13 @@
+module Hyrax
+  # Mailer for contacting the administrator
+  class ContactMailer < HykuMailer
+    def contact(contact_form)
+      @contact_form = contact_form
+      # Check for spam
+      return if @contact_form.spam?
+      headers = @contact_form.headers.dup
+      headers[:subject] << " [#{host_for_tenant}]"
+      mail(headers)
+    end
+  end
+end
