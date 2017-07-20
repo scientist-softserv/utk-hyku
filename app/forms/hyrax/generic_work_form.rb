34cdb7f5f91072bf07853f6177afe67f8685287f
Julie Allinson <julie.allinson@london.ac.uk>
2017-07-20 20:23:09 +0100

    Support downloads (eg. a PDF of the whole paged item) via Universal Viewer to fix #1372. Added 'rendering' property and a new method to the manifest_enabled_work_show_presenter to pass the rendering into iiif_manifest.

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -4,6 +4,10 @@ module Hyrax
   class GenericWorkForm < Hyrax::Forms::WorkForm
     self.model_class = ::GenericWork
     include HydraEditor::Form::Permissions
-    self.terms += [:resource_type]
+    self.terms += [:resource_type, :rendering_ids]
+
+    def secondary_terms
+      super - [:rendering_ids]
+    end
   end
 end
--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -1,6 +1,7 @@
 class GenericWork < ActiveFedora::Base
   include ::Hyrax::WorkBehavior
   include ::Hyrax::BasicMetadata
+  include HasRendering
   validates :title, presence: { message: 'Your work must have a title.' }
 
   # This indexer uses IIIF thumbnails:
