6d4f67427682a1f617db3d49ce3db8f77d25cea4
Claire <claire.dambrosio@ubiquitypress.com>
2018-06-28 15:06:59 +0100

    Added tests, corrected typo, corrected double negative syntax, improved isbn_regex which was catching series of 9 digits occasionally found in DOIs

--- spec/factories/generic_works.rb
+++ spec/factories/generic_works.rb
@@ -6,6 +6,11 @@ FactoryGirl.define do
 
     title ["Test title"]
 
+    identifier %w[
+      ISBN:978-83-7659-303-6 978-3-540-49698-4 9790879392788
+      doi:10.1038/nphys1170 3-921099-34-X 3-540-49698-x 0-19-852663-6
+    ]
+
     factory :work_with_one_file do
       before(:create) do |work, evaluator|
         work.ordered_members << FactoryGirl.create(:file_set,
--- spec/presenters/hyku/manifest_enabled_work_show_presenter_spec.rb
+++ spec/presenters/hyku/manifest_enabled_work_show_presenter_spec.rb
@@ -58,27 +58,80 @@ RSpec.describe Hyku::ManifestEnabledWorkShowPresenter do
     end
   end
 
-  context "when the work has identifier" do
-    let(:document) do
-      { "identifier_tesim" => %w[
-        ISBN:978-83-7659-303-6 978-3-540-49698-4 9790879392788
-        doi:10.1038/nphys1170 3-921099-34-X 3-540-49698-x 0-19-852663-6
-      ] }
-    end
-
+  context "when the work has valid doi and isbns" do
+    # the values are set in generic_works factory
     describe "#doi" do
       it "extracts the DOI from the identifiers" do
-        # puts solr_document.inspect
-        expect(presenter.doi).to eq("10.1038/nphys1170")
+        expect(presenter.doi).to eq('10.1038/nphys1170')
       end
     end
 
     describe "#isbns" do
       it "extracts ISBNs from the identifiers" do
-        expect(presenter.isbns).to eq(%w[
-                                        978-83-7659-303-6 978-3-540-49698-4 9790879392788
-                                        3-921099-34-X 3-540-49698-x 0-19-852663-6
-                                      ])
+        expect(presenter.isbns)
+          .to include('978-83-7659-303-6', '978-3-540-49698-4', '9790879392788',
+                      '3-921099-34-X', '3-540-49698-x', '0-19-852663-6')
+      end
+    end
+  end
+
+  context "when the identifier is nil" do
+    let(:document) do
+      { "identifier_tesim" => nil }
+    end
+
+    describe "#doi" do
+      it "is nil" do
+        expect(presenter.doi).to be_nil
+      end
+    end
+
+    describe "#isbns" do
+      it "is nil" do
+        expect(presenter.isbns).to be_nil
+      end
+    end
+  end
+
+  context "when the work has a doi only" do
+    let(:document) do
+      { "identifier_tesim" => ['10.1038/nphys1170'] }
+    end
+
+    describe "#isbns" do
+      it "is empty" do
+        expect(presenter.isbns).to be_empty
+      end
+    end
+  end
+
+  context "when the work has isbn(s) only" do
+    let(:document) do
+      { "identifier_tesim" => ['ISBN:978-83-7659-303-6'] }
+    end
+
+    describe "#doi" do
+      it "is empty" do
+        expect(presenter.doi).to be_empty
+      end
+    end
+  end
+
+  context "when the work's identifiers are not valid doi or isbns" do
+    # FOR CONSIDERATION: validate format when a user adds an identifier?
+    let(:document) do
+      { "identifier_tesim" => %w[3207/2959859860 svnklvw24 0470841559.ch1] }
+    end
+
+    describe "#doi" do
+      it "is empty" do
+        expect(presenter.doi).to be_empty
+      end
+    end
+
+    describe "#isbns" do
+      it "is empty" do
+        expect(presenter.isbns).to be_empty
       end
     end
   end
