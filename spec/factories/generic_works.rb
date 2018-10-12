7bd263706531056939b3b3fb110654eb1ea053b7
nicholas <woodward.nicholas@gmail.com>
2018-10-12 13:56:41 -0500

    rubocop fix

--- spec/factories/generic_works.rb
+++ spec/factories/generic_works.rb
@@ -6,11 +6,12 @@ FactoryBot.define do
 
     title { ["Test title"] }
 
-    identifier { %w[
+    identifier do
+      %w[
         ISBN:978-83-7659-303-6 978-3-540-49698-4 9790879392788
         doi:10.1038/nphys1170 3-921099-34-X 3-540-49698-x 0-19-852663-6
       ]
-    }
+    end
 
     factory :work_with_one_file do
       before(:create) do |work, evaluator|
