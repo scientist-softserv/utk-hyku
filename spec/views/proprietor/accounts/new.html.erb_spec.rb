d7399d3583fdb82f721ad0bc5892ea5965dddb43
nicholas <woodward.nicholas@gmail.com>
2018-10-12 13:44:27 -0500

    alignment errors, according to travis

--- spec/views/proprietor/accounts/new.html.erb_spec.rb
+++ spec/views/proprietor/accounts/new.html.erb_spec.rb
@@ -2,7 +2,7 @@ RSpec.describe "proprietor/accounts/new", type: :view do
   before do
     assign(:account, Account.new(
                        name: "MyString"
-                     ))
+    ))
   end
 
   it "renders new account form" do
