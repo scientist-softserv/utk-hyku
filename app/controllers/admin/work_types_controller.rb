c7cf346c76975cb68c5b0265235fa4c87b662117
sephirothkod <bjustice@uvic.ca>
2018-12-20 11:43:23 -0800

    add tenant specific work types

---
+++ app/controllers/admin/work_types_controller.rb
@@ -0,0 +1,29 @@
+module Admin
+  class WorkTypesController < ApplicationController
+    layout 'dashboard'
+
+    before_action do
+      authorize! :manage, Hyrax::Feature
+    end
+
+    def edit
+      site
+    end
+
+    def update
+      site.available_works = params[:available_works]
+      if site.save
+        flash[:notice] = "Work types have been successfully updated"
+      else
+        flash[:error] = "Work types were not updated"
+      end
+      render :action => "edit"
+    end
+
+    private
+
+    def site
+      @site ||= Site.first
+    end
+  end
+end
