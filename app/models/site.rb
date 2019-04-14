15e21f828ce0fbcc1f04bf04af20923e035660a8
Rob Kaufman <rob@notch8.com>
2019-04-13 23:23:45 -0700

    when a new site is created, enable all work types by default

--- app/models/site.rb
+++ app/models/site.rb
@@ -16,7 +16,9 @@ class Site < ApplicationRecord
 
     def instance
       return NilSite.instance if Account.global_tenant?
-      first_or_create
+      first_or_create do |site|
+        site.available_works = Hyrax.config.registered_curation_concern_types
+      end
     end
   end
 
