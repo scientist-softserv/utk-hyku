d3a8277e094f1f17125109e198f3d5a86ae1b2cc
Daniel Lim <daniel@notch8.com>
2020-04-13 17:30:58 +0000

    Merge branch 'i100-contact-us' into 'master'
    I100 contact us
    
    See merge request notch8/hyku-up!32

---
+++ spec/features/contact_spec.rb
@@ -0,0 +1,21 @@
+RSpec.describe 'Site contact configuration' do
+  context 'as an administrator' do
+    let(:user) { FactoryBot.create(:admin) }
+
+    before do
+      login_as(user, scope: :user)
+    end
+
+    describe 'application name' do
+      it 'updates the application name in the brand bar' do
+        visit edit_site_contact_path
+        fill_in 'Contact email', with: 'contact@email.com'
+        click_on 'Save'
+        # TODO(@dlim87): this is another test where capybara is getting logged out after an action
+        expect(page).to have_current_path(edit_site_contact_path)
+        expect(page).to have_field('Contact email')
+      end
+
+    end
+  end
+end
---
+++ spec/models/hyrax/contact_form_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe Hyrax::ContactForm, type: :model do
+  describe 'headers' do
+    before do
+      subject { described_class.new(
+        subject: "subject",
+        from: "from@email.com"
+      )}
+      allow(Hyrax.config).to receive(:contact_email).and_return('hyrax@email.com')
+    end
+
+    context 'no email set' do
+      before do
+        site = double(Site.new, contact_email: '')
+        allow(Site).to receive(:instance).and_return(site)
+      end
+      it 'uses the hyrax setting' do
+        byebug
+        expect(subject.headers[:to]).to eq('hyrax@email.com')
+      end
+    end
+    context 'site email set' do
+      before do
+        site = double(Site.new, contact_email: 'setting@email.com')
+        allow(Site).to receive(:instance).and_return(site)
+      end
+      it 'uses the Site email' do
+        expect(subject.headers[:to]).to eq('setting@email.com')
+      end
+    end
+  end
+end
