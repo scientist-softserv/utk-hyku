cda1626563f4d5be9e6f89629ed9524707e57aee
Bertie Wooles <bertie.wooles@gmail.com>
2020-12-04 16:00:28 +0000

    Adds jobs that run at Midnight everyday to expire embargos and leases (#1648)
    * Adds jobs that run at Midnight everyday to expire embargos and leases
    
    * Fixes Rubocop errors
    
    * Removes sidekiq-cron and replaces it with a DIY implementation
    
    * Deletes unused schedule yml
    
    * Uncomments method call and changes jobs to perform_later
    
    * remove sidekiq-cron
    
    Co-authored-by: Rob Kaufman <rob@notch8.com>

---
+++ spec/jobs/embargo_auto_expiry_job_spec.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+RSpec.describe EmbargoAutoExpiryJob do
+  before do
+    ActiveJob::Base.queue_adapter = :test
+  end
+
+  after do
+    clear_enqueued_jobs
+  end
+
+  let(:past_date) { 2.days.ago }
+  let(:future_date) { 2.days.from_now }
+
+  let(:account) { create(:account) }
+
+  let!(:embargoed_work) do
+    build(:work, embargo_release_date: future_date.to_s,
+                 visibility_during_embargo: 'restricted',
+                 visibility_after_embargo: 'open').tap do |work|
+      work.save(validate: false)
+    end
+  end
+
+  let!(:work_with_expired_embargo) do
+    build(:work, embargo_release_date: past_date.to_s,
+                 visibility_during_embargo: 'restricted',
+                 visibility_after_embargo: 'open').tap do |work|
+      work.save(validate: false)
+    end
+  end
+
+  let!(:file_set_with_expired_embargo) do
+    build(:file_set, embargo_release_date: past_date.to_s,
+                     visibility_during_embargo: 'restricted',
+                     visibility_after_embargo: 'open').tap do |file_set|
+      file_set.save(validate: false)
+    end
+  end
+
+  describe '#reenqueue' do
+    it 'Enques an EmbargoExpiryJob after perform' do
+      expect { EmbargoAutoExpiryJob.perform_now(account) }.to have_enqueued_job(EmbargoAutoExpiryJob)
+    end
+  end
+
+  describe '#perform' do
+    it "Expires the Embargo on a work with expired Embargo" do
+      expect(work_with_expired_embargo.visibility).to eq('restricted')
+      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+      EmbargoAutoExpiryJob.perform_now(account)
+      work_with_expired_embargo.reload
+      expect(work_with_expired_embargo.visibility).to eq('open')
+    end
+
+    it 'Expires embargos on file sets with expired embargos' do
+      expect(file_set_with_expired_embargo.visibility).to eq('restricted')
+      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+      EmbargoAutoExpiryJob.perform_now(account)
+      file_set_with_expired_embargo.reload
+      expect(file_set_with_expired_embargo.visibility).to eq('open')
+    end
+
+    it "Does not expire embargo when embargo is still active" do
+      expect(embargoed_work.visibility).to eq('restricted')
+      ActiveJob::Base.queue_adapter.perform_enqueued_jobs = true
+      EmbargoAutoExpiryJob.perform_now(account)
+      embargoed_work.reload
+      expect(embargoed_work.visibility).to eq('restricted')
+    end
+  end
+end
--- spec/services/create_account_spec.rb
+++ spec/services/create_account_spec.rb
@@ -45,4 +45,12 @@ RSpec.describe CreateAccount do
       expect(account2.account.errors).to match a_hash_including(:cname)
     end
   end
+
+  describe '#schedule_recurring_jobs' do
+    it "Enques Embargo and Lease Expiry jobs" do
+      expect(EmbargoAutoExpiryJob).to receive(:perform_later).with(account)
+      expect(LeaseAutoExpiryJob).to receive(:perform_later).with(account)
+      subject.schedule_recurring_jobs
+    end
+  end
 end
