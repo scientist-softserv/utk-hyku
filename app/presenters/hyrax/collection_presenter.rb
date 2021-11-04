6fd75cb22a9c2a086135862a99d225169ad89e36
summer-cook <summercook@bellsouth.net>
2021-11-04 13:57:03 -0600

    rubocop fixes

--- app/presenters/hyrax/collection_presenter.rb
+++ app/presenters/hyrax/collection_presenter.rb
@@ -1,16 +1,23 @@
-  # OVERRIDE FILE from Hyrax v2.9.0
-#
+# frozen_string_literal: true
+# OVERRIDE FILE from Hyrax v2.9.0
 # Override this class using #class_eval to avoid needing to copy the entire file over from
 # the dependency. For more info, see the "Overrides using #class_eval" section in the README.
 require_dependency Hyrax::Engine.root.join('app', 'presenters', 'hyrax', 'collection_presenter').to_s
-
 Hyrax::CollectionPresenter.class_eval do
-
   # Terms is the list of fields displayed by
   # app/views/collections/_show_descriptions.html.erb
   # OVERRIDE Hyrax - removed size
   def self.terms
-    %i[total_items resource_type creator contributor keyword license publisher date_created subject language identifier based_near related_url]
+    %i[ total_items 
+        resource_type 
+        creator contributor 
+        keyword license 
+        publisher 
+        date_created 
+        subject language 
+        identifier 
+        based_near 
+        related_url ]
   end
 
   def [](key)
