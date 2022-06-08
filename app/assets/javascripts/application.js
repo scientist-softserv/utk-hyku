a1aee2216657bcb887dc2a498889d42c614b9293
Selam Habteab <selam@scientist.com>
2022-06-08 23:53:24 +0000

    i45 enable bulkrax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,7 +41,7 @@
 //= require bootstrap-datepicker
 
 ///////////// BULKRAX - add eq sign to enable
-//require bulkrax/application
+//= require bulkrax/application
 /////////////////////////////////////////////
 
 //= require hyrax
