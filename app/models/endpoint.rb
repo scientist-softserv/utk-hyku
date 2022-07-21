a91e051086c306801b273556f1dfad67df6290ed
Shana Moore <shana.lavina.moore@gmail.com>
2022-07-21 17:59:32 +0000

    Rename fedora url to fix pipeline

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -4,7 +4,7 @@ class Endpoint < ApplicationRecord
   has_one :account
 
   # if an option is blank on an endpoint, the whole app process will crash
-  # when going to that tenant as fedora_url becomes blank. This code
+  # when going to that tenant as fcrepo_url becomes blank. This code
   # prevents that and other nastiness in the event of added or removed
   # options or when data gets converted from one format to another between
   # JSON and a Ruby hash
