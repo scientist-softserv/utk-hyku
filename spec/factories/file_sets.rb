35c54cb3a7c5ccb3dcf6b699f5e367dcd1b46508
Kirk Wang <kirk.wang@scientist.com>
2022-10-07 21:12:52 +0000

    adjust-factories-2

--- spec/factories/file_sets.rb
+++ spec/factories/file_sets.rb
@@ -4,14 +4,19 @@ FactoryBot.define do
   factory :file_set do
     transient do
       user { FactoryBot.create(:user) }
-    end
-
-    trait :image do
-      content { File.open(Hyrax::Engine.root + 'spec/fixtures/world.png') }
+      content { nil }
     end
 
     after(:build) do |fs, evaluator|
       fs.apply_depositor_metadata evaluator.user
+    end
+
+    after(:create) do |file, evaluator|
+      Hydra::Works::UploadFileToFileSet.call(file, evaluator.content) if evaluator.content
+    end
+
+    trait :image do
+      content { File.open(Rails.root.join('spec', 'fixtures', 'world.png')) }
     end
   end
 end
