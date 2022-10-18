15b8db10da83eadbcdcefa53b7a0b8bd52cdca05
Shana Moore <shana.lavina.moore@gmail.com>
2022-10-18 16:38:59 +0000

    Part 2: Create new model called Attachment

---
+++ app/actors/hyrax/actors/attachment_actor.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# Generated via
+#  `rails generate hyrax:work Attachment`
+module Hyrax
+  module Actors
+    class AttachmentActor < Hyrax::Actors::BaseActor
+    end
+  end
+end
