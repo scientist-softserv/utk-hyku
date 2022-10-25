440424ca4b4b10be1efe9cc806a8acb484e05f67
Shana <shana.lavina.moore@gmail.com>
2022-10-25 09:24:37 -0700

    229 - handle nil case and casue of sidekiq sentry error

---
+++ app/jobs/content_event_job_decorator.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+ 
+# OVERRIDE HYRAX 3.4.1 to handle nil case and casue of sidekiq sentry error
+# TODO: why is depositor coming across as nil?
+module ContentEventJobDecorator
+  # OVERRIDE HYRAX 3.4.1 to handle nil case and casue of sidekiq sentry error
+  # log the event to the users profile stream
+  def log_user_event(depositor)
+    depositor&.log_profile_event(event)
+  end
+end
+
+ContentEventJob.prepend(ContentEventJobDecorator)
