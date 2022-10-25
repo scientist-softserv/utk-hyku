781422bbbc73692e9028cd658cfb71de95ddafed
Shana <shana.lavina.moore@gmail.com>
2022-10-25 12:57:03 -0700

    299 - override Hyrax::VisibilityPropagator::NullVisibilityPropogator

--- app/services/hyrax/visibility_propagator_decorator.rb
+++ app/services/hyrax/visibility_propagator_decorator.rb
@@ -2,25 +2,18 @@
 
 ## OVERRIDE HYRAX 3.4.1 to correct method spelling that causes sidekiq errors.
 
-module Hyrax
-  ##
-  # @abstract Propagates visibility from a provided object (e.g. a Work) to some
-  #   group of its members (e.g. file_sets).
-  module VisibilityPropagatorDecorator
-    ## OVERRIDE HYRAX 3.4.1 to correct method spelling that causes sidekiq errors.
-    # @return [void]
-    # @raise [RuntimeError] if we're in development mode
-    def propagate
-      # rubocop:disable Performance/Caller
-      message = "Tried to propagate visibility to members of #{source} " \
-                  "but didn't know what kind of object it is. Model " \
-                  "name #{source.try(:model_name)}. Called from #{caller[0]}."
-      # rubocop:enable Performance/Caller
+Hyrax::VisibilityPropagator::NullVisibilityPropogator.class_eval do
+  ## OVERRIDE HYRAX 3.4.1 to correct method spelling that causes sidekiq errors.
+  # @return [void]
+  # @raise [RuntimeError] if we're in development mode
+  def propagate
+    # rubocop:disable Performance/Caller
+    message = "Tried to propagate visibility to members of #{source} " \
+                "but didn't know what kind of object it is. Model " \
+                "name #{source.try(:model_name)}. Called from #{caller[0]}."
+    # rubocop:enable Performance/Caller
 
-      Hyrax.logger.warn(message)
-      Rails.env.development? ? raise(message) : :noop
-    end
+    Hyrax.logger.warn(message)
+    Rails.env.development? ? raise(message) : :noop
   end
 end
-
-Hyrax::VisibilityPropagator.prepend(Hyrax::VisibilityPropagatorDecorator)
