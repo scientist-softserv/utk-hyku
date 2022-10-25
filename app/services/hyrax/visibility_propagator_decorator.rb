b011efdc180576ac6cfd06034e3bac646ad890c0
Shana <shana.lavina.moore@gmail.com>
2022-10-25 09:24:37 -0700

    229 - handle nil case and casue of sidekiq sentry error

--- app/services/hyrax/visibility_propagator_decorator.rb
+++ app/services/hyrax/visibility_propagator_decorator.rb
@@ -6,17 +6,16 @@ module Hyrax
   # @abstract Propagates visibility from a provided object (e.g. a Work) to some
   #   group of its members (e.g. file_sets).
   module VisibilityPropagatorDecorator
-      ## OVERRIDE HYRAX 3.4.1 to correct method spelling that causes sidekiq errors.
-      # @return [void]
-      # @raise [RuntimeError] if we're in development mode
-      def propagate
-        message =  "Tried to propagate visibility to members of #{source} " \
-                   "but didn't know what kind of object it is. Model " \
-                   "name #{source.try(:model_name)}. Called from #{caller[0]}."
+    ## OVERRIDE HYRAX 3.4.1 to correct method spelling that causes sidekiq errors.
+    # @return [void]
+    # @raise [RuntimeError] if we're in development mode
+    def propagate
+      message =  "Tried to propagate visibility to members of #{source} " \
+                  "but didn't know what kind of object it is. Model " \
+                  "name #{source.try(:model_name)}. Called from #{caller[0]}."
 
-        Hyrax.logger.warn(message)
-        Rails.env.development? ? raise(message) : :noop
-      end
+      Hyrax.logger.warn(message)
+      Rails.env.development? ? raise(message) : :noop
     end
   end
 end
