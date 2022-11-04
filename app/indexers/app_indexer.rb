54e51a3f3eb2075a6a2ae55367866a13f03ef692
April Rieger <63515648+aprilrieger@users.noreply.github.com>
2022-11-03 17:06:53 -0700

    add return to get a commit to run the workflows
    add return to get a commit to run the workflows

--- app/indexers/app_indexer.rb
+++ app/indexers/app_indexer.rb
@@ -5,6 +5,7 @@ class AppIndexer < Hyrax::WorkIndexer
   # provide your own metadata and indexing.
   # include Hyrax::IndexesBasicMetadata
 
+
   # Fetch remote labels for objects with controlled properties (i.e. :based_near)
   # You can remove this if you don't want this behavior
   include Hyrax::IndexesLinkedMetadata
