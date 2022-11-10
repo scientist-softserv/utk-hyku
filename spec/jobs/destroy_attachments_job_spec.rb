06c375a7372df79273c71409b2e4c778071574cb
Kirk Wang <kirk.wang@scientist.com>
2022-11-09 17:12:08 -0800

    refactor and add more tests

--- spec/jobs/destroy_attachments_job_spec.rb
+++ spec/jobs/destroy_attachments_job_spec.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe DestroyAttachmentsJob, type: :job do
-  let(:attachment) { FactoryBot.create(:attachment_with_one_file) }
-  let(:work) { FactoryBot.create(:generic_work) }
+  let(:work) { FactoryBot.create(:generic_work_with_one_attachment) }
 
-  before do
-    allow(work).to receive(:member_ids).and_return([attachment.id])
-  end
-
-  it 'calls the job' do
-    expect(described_class).to receive(:perform_later).with([attachment.id]).once
+  it 'gets called when a work is destroyed' do
+    expect(described_class).to receive(:perform_later).with(work.member_ids).once
     work.destroy
   end
 
   describe '#perform' do
+    before do
+      work.destroy
+    end
+
     let(:run_job) { described_class.perform_now(work.member_ids) }
 
     context 'with one Attachment child work' do
@@ -27,11 +26,7 @@ RSpec.describe DestroyAttachmentsJob, type: :job do
     end
 
     context 'with multiple Attachments' do
-      let(:another_attachment) { FactoryBot.create(:attachment_with_one_file) }
-
-      before do
-        allow(work).to receive(:member_ids).and_return([attachment.id, another_attachment.id])
-      end
+      let(:work) { FactoryBot.create(:generic_work_with_two_attachments) }
 
       it 'deletes all Attachments' do
         expect { run_job }.to change(Attachment, :count).by(-2)
@@ -39,6 +34,25 @@ RSpec.describe DestroyAttachmentsJob, type: :job do
 
       it 'deletes all FileSets of the Attachments' do
         expect { run_job }.to change(FileSet, :count).by(-2)
+      end
+    end
+
+    context 'with two works that have the same Attachment' do
+      let(:another_work) { FactoryBot.create(:generic_work) }
+
+      it 'will not delete the Attachment if a parent still exists' do
+        another_work.ordered_members << work.members.first
+        another_work.save
+
+        expect { run_job }.not_to change(Attachment, :count)
+      end
+    end
+
+    context 'with one Attachment and a non-Attachment' do
+      let(:work) { FactoryBot.create(:generic_work_with_one_attachment_and_one_image) }
+
+      it 'will only delete the Attachment and leave the non-Attachment' do
+        expect { run_job }.to change(Attachment, :count).by(-1).and change(Image, :count).by(0)
       end
     end
   end
