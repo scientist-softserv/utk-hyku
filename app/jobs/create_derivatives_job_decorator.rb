bac07f742874a50306319a6ca52d13753e93b9d7
Shana <shana.lavina.moore@gmail.com>
2022-11-22 14:48:31 -0800

    [i225] logic refactor to check for presence of a word in a combined string

--- app/jobs/create_derivatives_job_decorator.rb
+++ app/jobs/create_derivatives_job_decorator.rb
@@ -19,7 +19,7 @@ module Hyrax
         return false
       end
 
-      return if file_set.rdf_type.select{ |file| file&.downcase&.include?(INTERMEDIATE_FILE) }.nil?
+      return unless file_set.rdf_type.join&.downcase&.include?(INTERMEDIATE_FILE)
 
       # Ensure a fresh copy of the repo file's latest version is being worked on, if no filepath is directly provided
       unless filepath && File.exist?(filepath)
