b488afd19aa3070cac8f927e41ee0eef07346e34
Kirk Wang <kirk.wang@scientist.com>
2022-12-01 11:38:40 -0800

    use original size from file for the size attribute

--- config/initializers/file_set_derivatives_overrides.rb
+++ config/initializers/file_set_derivatives_overrides.rb
@@ -37,5 +37,16 @@ Hyrax::FileSetDerivativesService.class_eval do
                                                             url: derivative_url('thumbnail'),
                                                             layer: 0 }])
   end
-  
+
+  def create_video_derivatives(filename)
+    original_size = "#{file_set.width.first}x#{file_set.height.first}"
+    Hydra::Derivatives::Processors::Video::Processor.config.size_attributes = original_size
+    Hydra::Derivatives::VideoDerivatives.create(filename,
+                                                outputs: [{ label: :thumbnail, format: 'jpg',
+                                                            url: derivative_url('thumbnail') },
+                                                          { label: 'webm', format: 'webm',
+                                                            url: derivative_url('webm') },
+                                                          { label: 'mp4', format: 'mp4',
+                                                            url: derivative_url('mp4') }])
+  end
 end
