7d628f65b50bdb0c2c3b9301267079c445a111c5
LaRita Robinson <larita@scientist.com>
2022-12-02 12:02:08 -0500

    Lint fixes

--- app/jobs/content_event_job_decorator.rb
+++ app/jobs/content_event_job_decorator.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 # OVERRIDE Hyrax 3.x - Override log_user_event to avoid calling on nil user
-# Hyrax::Collections::CollectionMemberService.add_members_by_ids expects a user, 
-# which defaults to nil when called by the deprecated add_members method which is used in Bulkrax's CreateRelationships job.
+# Hyrax::Collections::CollectionMemberService.add_members_by_ids expects a user,
+# which defaults to nil when called by the deprecated add_members method
+# which is used in Bulkrax's CreateRelationships job.
 module ContentEventJobDecorator
   # log the event to the users profile stream
   def log_user_event(depositor)
-    depositor.log_profile_event(event) if depositor
+    depositor&.log_profile_event(event)
   end
 end
 
