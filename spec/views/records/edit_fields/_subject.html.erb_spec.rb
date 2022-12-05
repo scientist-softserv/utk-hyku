5c16140343deb03606e008e4bb7ecff4d6e42ebd
Shana Moore <shana.lavina.moore@gmail.com>
2022-12-05 10:44:33 -0800

    I251 subject search box (#238)
    * [i212] - connect LOC to remote qa
    
    * [i212] - add subject partial for controlled vocabulary and spec
    
    * rubocop fix

---
+++ spec/views/records/edit_fields/_subject.html.erb_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe 'records/edit_fields/_subject.html.erb', type: :view do
+  let(:work) { GenericWork.new }
+  let(:form) { Hyrax::GenericWorkForm.new(work, nil, controller) }
+  let(:form_template) do
+    %(
+      <%= simple_form_for [main_app, @form] do |f| %>
+        <%= render "records/edit_fields/subject", f: f, key: 'subject' %>
+      <% end %>
+    )
+  end
+
+  before do
+    assign(:form, form)
+    render inline: form_template
+  end
+
+  it 'has url for autocomplete service' do
+    expect(rendered).to have_selector(
+      'input[data-autocomplete-url="/authorities/search/loc/subjects"][data-autocomplete="subject"]'
+    )
+  end
+end
