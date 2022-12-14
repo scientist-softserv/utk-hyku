06001661a9f4d4af0a0cb3d0112aebdd54b5133a
Shana <shana.lavina.moore@gmail.com>
2022-12-14 14:25:09 -0800

    Create application_mathcer_decorator.rb

---
+++ app/matchers/bulkrax/application_mathcer_decorator.rb
@@ -0,0 +1,26 @@
+# fronen_string_literatl: true
+
+# OVERRIDE BULKRAX 4.4.0 to remove resource_type from #process_parse
+# this app redefined resouce_types.yml to be inline with local questioning
+# authority
+module Bulkrax
+  module ApplicationMatcherDecorator
+    def process_parse
+      # New parse methods will need to be added here
+      # OVERRIDE BULKRAX 4.4.0 to remove resource_type from #process_parse
+      parsed_fields = ['remote_files', 'language', 'subject', 'types', 'model', 'format_original']
+      # This accounts for prefixed matchers
+      parser = parsed_fields.find { |field| to&.include? field }
+      if @result.is_a?(Array) && self.parsed && self.respond_to?("parse_#{parser}")
+        @result.each_with_index do |res, index|
+          @result[index] = send("parse_#{parser}", res.strip)
+        end
+        @result.delete(nil)
+      elsif self.parsed && self.respond_to?("parse_#{parser}")
+        @result = send("parse_#{parser}", @result)
+      end
+    end
+  end
+end
+
+Bulkrax::ApplicationMatcher.prepend(Bulkrax::ApplicationMatcherDecorator)
