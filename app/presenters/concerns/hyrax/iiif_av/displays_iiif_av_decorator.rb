93746ce3046314e4df703a952d828543a1fef74e
Kirk Wang <kirk.wang@scientist.com>
2023-03-17 16:07:11 -0700

    Bring in a change for displaying the viewer
    This commit was used in BL and now here to have the viewer display.
    Primary reason is because of the AV gem and IIIF Print being installed.

--- app/presenters/concerns/hyrax/iiif_av/displays_iiif_av_decorator.rb
+++ app/presenters/concerns/hyrax/iiif_av/displays_iiif_av_decorator.rb
@@ -1,10 +1,19 @@
 Hyrax::IiifAv::DisplaysIiifAv.module_eval do
-    # @return [Boolean] render a IIIF viewer
-    def iiif_viewer?
-      (representative_id.present? &&
-        representative_presenter.present? &&
-        (representative_presenter.video? ||
-          representative_presenter.audio? ||
-          (representative_presenter.image? && Hyrax.config.iiif_image_server?))) || super
-    end
+  def iiif_viewer?
+    # Hyrax::IiifAv overrides Hyrax, to add in audio & video files
+    # Here we override Hyrax::IiifAv::DisplaysIiifAv to use
+    # IiifPrint::WorkShowPresenterDecorator to also include child work files
+    super
   end
+
+  private
+
+    # Overriding iiif_print to include additional file types
+    # TODO: remove once config options are available in iiif_print
+    def file_type_and_permissions_valid?(presenter)
+      current_ability.can?(:read, presenter.id) &&
+        (presenter.try(:image?) || presenter.try(:solr_document).try(:image?) ||
+        presenter.try(:video?) || presenter.try(:solr_document).try(:video?) ||
+        presenter.try(:audio?) || presenter.try(:solr_document).try(:audio?))
+    end
+end
