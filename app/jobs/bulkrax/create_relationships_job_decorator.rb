752f758039355fb8bc06d04492959a7b56441e04
Kirk Wang <kirk.wang@scientist.com>
2023-06-23 15:45:48 -0700

    🎁 Automatically add thumbnails to non-pdf files
    This commit hooks into `Bulkrax::Jobs::CreateRelationshipsJob` to
    trigger the `SetDefaultParentThumbnailJob` for non-pdf files.  The two
    jobs are very similar yet different enough to make DRYing it up a bit
    complicated so I opted for creating a separate decorator.
    
    Ref:
      - https://github.com/scientist-softserv/utk-hyku/issues/438

--- app/jobs/bulkrax/create_relationships_job_decorator.rb
+++ app/jobs/bulkrax/create_relationships_job_decorator.rb
@@ -2,26 +2,20 @@
 
 # OVERRIDE IIIF Print v1.0.0 to call SetDefaultParentThumbnailJob
 
-module IiifPrint
+module Bulkrax
   module Jobs
     module CreateRelationshipsJobDecorator
-      def add_to_work(child_record:, parent_record:)
+      def add_to_work(child_record, parent_record)
         super
         set_default_parent_thumbnail(child_record, parent_record) if child_record&.intermediate_file?
       end
 
       def set_default_parent_thumbnail(child_record, parent_record)
-        return unless first_page?(child_record.title.first)
-
         ::SetDefaultParentThumbnailJob.set(wait: 5.minutes)
                                       .perform_later(child_work: child_record, parent_work: parent_record)
-      end
-
-      def first_page?(title)
-        title.match(/Page\s0*1\Z/)
       end
     end
   end
 end
 
-IiifPrint::Jobs::CreateRelationshipsJob.prepend(IiifPrint::Jobs::CreateRelationshipsJobDecorator)
+Bulkrax::Jobs::CreateRelationshipsJob.prepend(Bulkrax::Jobs::CreateRelationshipsJobDecorator)
