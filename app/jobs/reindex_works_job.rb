1f676ac3022937a473c6f540d995b6b76a28e140
LaRita Robinson <larita@scientist.com>
2023-07-31 15:12:31 -0400

    Submit work reindex jobs individually
    Refs #516
    Reindex works job can crash with large amounts of data. This splits
    the reindexing into one job per work to prevent memory leaks.

--- app/jobs/reindex_works_job.rb
+++ app/jobs/reindex_works_job.rb
@@ -6,7 +6,9 @@ class ReindexWorksJob < ApplicationJob
       work.update_index
     else
       Hyrax.config.registered_curation_concern_types.each do |work_type|
-        work_type.constantize.find_each(&:update_index)
+        work_type.constantize.find_each do |work|
+          ReindexWorksJob.perform_later(work)
+        end
       end
     end
   end
