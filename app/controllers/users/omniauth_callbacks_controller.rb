4142237dead6dfa2b2af50c94b7afe979fbd7815
Rob Kaufman <rob@notch8.com>
2023-09-13 22:38:10 -0700

    small css and flow fixes for sso. do not put google p12 file in the repo

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,14 +12,9 @@ module Users
       if @user.persisted?
         # We need to render a loading page here just to set the sesion properly
         # otherwise the logged in session gets lost during the redirect
-        if params[:action] == 'saml'
-          set_flash_message(:notice, :success, kind: params[:action]) if is_navigational_format?
-          sign_in @user, event: :authentication # this will throw if @user is not activated
-          render 'complete'
-        else
-          sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
-          set_flash_message(:notice, :success, kind: params[:action]) if is_navigational_format?
-        end
+        set_flash_message(:notice, :success, kind: params[:action]) if is_navigational_format?
+        sign_in @user, event: :authentication # this will throw if @user is not activated
+        render 'complete'
       else
         session['devise.user_attributes'] = @user.attributes
         redirect_to new_user_registration_url
