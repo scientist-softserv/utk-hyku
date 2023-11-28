0c5e43fc0563e7cf83c077cb49ce8daa833dae24
Shana Moore <shana@scientist.com>
2023-11-28 08:20:38 -0800

    💄 rubocop fix

--- app/controllers/hyrax/downloads_controller_decorator.rb
+++ app/controllers/hyrax/downloads_controller_decorator.rb
@@ -6,12 +6,15 @@ require 'aws-sdk-s3'
 
 module Hyrax
   module DownloadsControllerDecorator
-
     def send_file_contents
       if ENV['S3_DOWNLOADS']
         s3_object = Aws::S3::Object.new(ENV['AWS_BUCKET'], file.digest.first.to_s.gsub('urn:sha1:', ''))
         if s3_object.exists?
-          redirect_to s3_object.presigned_url(:get, expires_in: 3600, response_content_disposition: "attachment\; filename=#{file.original_name}")
+          redirect_to s3_object.presigned_url(
+            :get,
+            expires_in: 3600,
+            response_content_disposition: "attachment\; filename=#{file.original_name}"
+          )
           return
         end
       end
