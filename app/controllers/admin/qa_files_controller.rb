86e254fadb495715be58a09e24fadccf40fdfefa
Rob Kaufman <rob@notch8.com>
2024-01-13 00:18:20 -0800

    rubocop fixes

--- app/controllers/admin/qa_files_controller.rb
+++ app/controllers/admin/qa_files_controller.rb
@@ -1,10 +1,13 @@
-class Admin::QaFilesController < AdminController
-  def csvs
-  end
+# frozen_string_literal: true
 
-  def generate_csvs
-    uqc = UniqueQaCsvs.new
-    uqc.run
-    send_file uqc.output_path + '/qa.zip'
+module Admin
+  class QaFilesController < AdminController
+    def csvs; end
+
+    def generate_csvs
+      uqc = UniqueQaCsvs.new
+      uqc.run
+      send_file uqc.output_path + '/qa.zip'
+    end
   end
 end
--- app/services/unique_qa_csvs.rb
+++ app/services/unique_qa_csvs.rb
@@ -2,19 +2,20 @@
 
 class UniqueQaCsvs
   attr_accessor :input_path, :output_path, :output_subpath, :datestamp
-  def initialize(input_path: Rails.root.join('spec/fixtures/utk/366-367'), output_path: Dir.mktmpdir)
+  def initialize(input_path: Rails.root.join('spec', 'fixtures', 'utk', '366-367'), output_path: Dir.mktmpdir)
     @input_path = File.expand_path(input_path)
     @output_path = File.expand_path(output_path)
     @output_subpath = @output_path + '/qa'
     FileUtils.mkdir(@output_subpath)
-    @datestamp = Time.now.strftime("%Y%m%d%H%M:")
+    @datestamp = Time.zone.now.strftime("%Y%m%d%H%M:")
   end
 
   def run
     Dir.glob("#{input_path}/*.csv").each do |csv_file|
-      puts "Processing #{csv_file}"
+      Rails.logger.debug "Processing #{csv_file}"
       CSV.open(csv_file, headers: true) do |in_csv|
-        CSV.open(File.join(output_subpath, File.basename(csv_file)), 'w', headers: in_csv.headers, write_headers: true) do |out_csv|
+        out_csv_path = File.join(output_subpath, File.basename(csv_file))
+        CSV.open(out_csv_path, 'w', headers: in_csv.headers, write_headers: true) do |out_csv|
           in_csv.each do |row|
             row['source_identifier'] = modify_value(row['source_identifier'])
             row['parents'] = modify_value(row['parents'])
@@ -24,7 +25,7 @@ class UniqueQaCsvs
       end
     end
 
-    %x[zip -r #{output_path}/qa.zip #{output_subpath}]
+    `zip -r #{output_path}/qa.zip #{output_subpath}`
   end
 
   def modify_value(value)
