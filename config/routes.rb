3440ac23abd4d861624b004d620900a77bad7ffa
Kirk Wang <kirk.wang@scientist.com>
2024-03-11 11:56:12 -0700

    🐛 Fix undefined local variable `iiif_print`
    There was a change in this version of IIIF Print that needed an update
    to routes.rb.
    
    Ref:
      - https://github.com/scientist-softserv/utk-hyku/issues/614
      - https://github.com/scientist-softserv/iiif_print/pull/302

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   concern :oai_provider, BlacklightOaiProvider::Routes.new
 
   mount Hyrax::IiifAv::Engine, at: '/'
+  mount IiifPrint::Engine, at: '/'
   mount Riiif::Engine => 'images', as: :riiif if Hyrax.config.iiif_image_server?
 
   authenticate :user, lambda { |u| u.is_superadmin || u.is_admin } do
