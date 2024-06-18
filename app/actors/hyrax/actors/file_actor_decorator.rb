e77a5af10a998a5ce7d26eb8e4bd647a003a380f
Rob Kaufman <rob@notch8.com>
2024-06-18 16:07:39 -0700

    fix rubocop

--- app/actors/hyrax/actors/file_actor_decorator.rb
+++ app/actors/hyrax/actors/file_actor_decorator.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 module Hyrax
   module Actors
     # Actions for a file identified by file_set and relation (maps to use predicate)
     # @note Spawns asynchronous jobs
     module FileActorDecorator
-
       def perform_ingest_file_through_active_fedora(io)
-        # Skip versioning because versions will be minted by VersionCommitter as necessary during save_characterize_and_record_committer.
+        # Skip versioning because versions will be minted by VersionCommitter as necessary during
+        # save_characterize_and_record_committer.
         # these are files too big to send to S3 w/o Streaming
         Rails.logger.error("[FileActor] starting write for #{file_set.id}")
         if io.size.to_i >= 3.gigabytes
@@ -18,18 +20,18 @@ module Hyrax
           # how do we make sure the sha gets indexed?
         else
           Rails.logger.error("[FileActor] writing to fcrepo #{file_set.id}")
-          # Skip versioning because versions will be minted by VersionCommitter as necessary during save_characterize_and_record_committer.
+          # Skip versioning because versions will be minted by VersionCommitter as necessary during
+          # save_characterize_and_record_committer.
           Hydra::Works::AddFileToFileSet.call(file_set,
-            io,
-            relation,
-            versioning: false)
+                                              io,
+                                              relation,
+                                              versioning: false)
         end
         return false unless file_set.save
         repository_file = related_file
         create_version(repository_file, user)
         CharacterizeJob.perform_later(file_set, repository_file.id, pathhint(io))
       end
-
     end
   end
 end
